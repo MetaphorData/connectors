1b3cc5e8bc3bbb4c785d1ca5fe81b67724ba3c15
Mars Lan <mars.th.lan@gmail.com>
2021-08-10 09:34:36 -0700

    Move S3 writing logic to a separate module to promote reusability (#5)

---
+++ metaphor/common/s3.py
@@ -0,0 +1,18 @@
+import json
+
+from smart_open import open
+
+# Give S3 bucket owner full control over the new object
+# See https://github.com/RaRe-Technologies/smart_open/blob/develop/howto.md#how-to-pass-additional-parameters-to-boto3
+OWNER_FULL_CONTROL_ACL = {
+    "client_kwargs": {
+        "S3.Client.put_object": {"ACL": "bucket-owner-full-control"},
+        "S3.Client.create_multipart_upload": {"ACL": "bucket-owner-full-control"},
+    }
+}
+
+
+def write_file(path: str, payload: object):
+    params = OWNER_FULL_CONTROL_ACL if path.startswith("s3://") else None
+    with open(path, "w", transport_params=params) as fp:
+        json.dump(payload, fp)
