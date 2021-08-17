71b80c993d332d9f55733511cc47455f64703c5b
Yi (Alan) Wang <yi@metaphor.io>
2021-08-17 08:05:25 -0700

    Retrieve Snowflake table level usage stats [ch2882] (#15)
    * Retrieve Snowflake table level usage stats [ch2882]
    
    * renamme module
    
    * Address comments
    
    * fix styling
    
    * add include/exclude filters for table names
    
    * fix style
    
    * Add quicktype issue link

---
+++ tests/snowflake/usage/test_config.py
@@ -0,0 +1,37 @@
+from metaphor.snowflake.usage.extractor import SnowflakeUsageRunConfig
+
+
+def test_json_config(test_root_dir):
+    config = SnowflakeUsageRunConfig.from_json_file(
+        f"{test_root_dir}/snowflake/usage/config.json"
+    )
+
+    assert config == SnowflakeUsageRunConfig(
+        account="account",
+        user="user",
+        password="password",
+        lookback_days=365,
+        included_databases={"DEMO_DB"},
+        excluded_usernames={"METAPHOR"},
+        included_table_names={"*.*"},
+        excluded_table_names={"*.databases"},
+        output=None,
+    )
+
+
+def test_yaml_config(test_root_dir):
+    config = SnowflakeUsageRunConfig.from_yaml_file(
+        f"{test_root_dir}/snowflake/usage/config.yml"
+    )
+
+    assert config == SnowflakeUsageRunConfig(
+        account="account",
+        user="user",
+        password="password",
+        lookback_days=30,
+        included_databases=set(),
+        excluded_usernames=set(),
+        included_table_names={"*"},
+        excluded_table_names=set(),
+        output=None,
+    )
