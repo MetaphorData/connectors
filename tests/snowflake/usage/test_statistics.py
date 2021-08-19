1bfcf1d320532385b88f1569a737e566cb90da4a
Yi Wang <yi@metaphor.io>
2021-08-19 09:39:13 -0700

    update tests

--- tests/snowflake/usage/test_statistics.py
+++ tests/snowflake/usage/test_statistics.py
@@ -24,32 +24,42 @@ def make_dataset_with_usage(counts: List[int]):
 
 
 def test_percentile():
-    dataset1 = make_dataset_with_usage([1, 2, 3, 4, 5])
-    dataset2 = make_dataset_with_usage([10, 20, 30, 40, 50])
-    dataset3 = make_dataset_with_usage([100, 200, 300, 400, 500])
+    dataset1 = make_dataset_with_usage([1, 3, 6, 8, 10])
+    dataset2 = make_dataset_with_usage([2, 3, 5, 10, 20])
+    dataset3 = make_dataset_with_usage([10, 20, 30, 40, 50])
 
     SnowflakeUsageExtractor.calculate_percentile(
         [dataset1, dataset2, dataset3],
         lambda dataset: dataset.usage.query_counts.last24_hours,
     )
 
+    SnowflakeUsageExtractor.calculate_percentile(
+        [dataset1, dataset2, dataset3],
+        lambda dataset: dataset.usage.query_counts.last7_days,
+    )
+
+    # check the calculated percentile for 24 hours
     assert dataset1.usage.query_counts.last24_hours == QueryCount(
         count=1, percentile=0.0
     )
-    assert dataset1.usage.query_counts.last7_days == QueryCount(count=2)
-    assert dataset1.usage.query_counts.last30_days == QueryCount(count=3)
-
     assert dataset2.usage.query_counts.last24_hours == QueryCount(
-        count=10, percentile=0.3333333333333333
+        count=2, percentile=0.3333333333333333
     )
-    assert dataset2.usage.query_counts.last7_days == QueryCount(count=20)
-    assert dataset2.usage.query_counts.last30_days == QueryCount(count=30)
-
     assert dataset3.usage.query_counts.last24_hours == QueryCount(
-        count=100, percentile=0.6666666666666666
+        count=10, percentile=0.6666666666666666
     )
-    assert dataset3.usage.query_counts.last7_days == QueryCount(count=200)
-    assert dataset3.usage.query_counts.last30_days == QueryCount(count=300)
+
+    # check the calculated percentile for 7 days
+    assert dataset1.usage.query_counts.last7_days == QueryCount(count=3, percentile=0.0)
+    assert dataset2.usage.query_counts.last7_days == QueryCount(count=3, percentile=0.0)
+    assert dataset3.usage.query_counts.last7_days == QueryCount(
+        count=20, percentile=0.6666666666666666
+    )
+
+    # the percentile will be None since we didn't call the calculation for 30 days
+    assert dataset1.usage.query_counts.last30_days == QueryCount(count=6)
+    assert dataset2.usage.query_counts.last30_days == QueryCount(count=5)
+    assert dataset3.usage.query_counts.last30_days == QueryCount(count=30)
 
 
 def test_percentile_only_1():
