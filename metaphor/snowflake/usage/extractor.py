d02698d09b34bdba24cae4c21900d38f8ce5d0c4
Yi (Alan) Wang <yi@metaphor.io>
2021-08-26 09:13:06 -0700

    Snowflake column level usage stats [ch2884] (#25)
    * Snowflake column level usage stats [ch2884]
    
    * fix typing

--- metaphor/snowflake/usage/extractor.py
+++ metaphor/snowflake/usage/extractor.py
@@ -2,9 +2,10 @@ import logging
 from dataclasses import dataclass, field
 from datetime import datetime, timedelta, timezone
 from fnmatch import fnmatch
-from typing import Callable, Collection, Dict, List, Set
+from typing import Callable, Collection, Dict, List, Set, Tuple
 
 from serde import deserialize
+from sql_metadata import Parser
 
 from metaphor.common.event_util import EventUtil
 
@@ -22,6 +23,8 @@ from metaphor.models.metadata_change_event import (
     DatasetLogicalID,
     DatasetUsage,
     EntityType,
+    FieldQueryCount,
+    FieldQueryCounts,
     MetadataChangeEvent,
     QueryCount,
     QueryCounts,
@@ -125,7 +128,7 @@ class SnowflakeUsageExtractor(BaseExtractor):
                 logger.debug(f"Queries: {queries}")
 
                 for query in queries:
-                    self._parse_query_tables(query[1], query[2], query[3], query[4])
+                    self._parse_query_log(query[1], query[2], query[3], query[4])
 
                 self.total_queries_count += len(queries)
                 logger.info(
@@ -142,20 +145,27 @@ class SnowflakeUsageExtractor(BaseExtractor):
 
         return [EventUtil.build_dataset_event(d) for d in self._datasets.values()]
 
-    def _parse_query_tables(
+    def _parse_query_log(
         self, query: str, db: str, schema: str, start_time: datetime
     ) -> None:
-        tables = []
         try:
-            tables = sql_metadata.Parser(query).tables
+            parser = sql_metadata.Parser(query)
+            tables = self._parse_tables(parser, db, schema, start_time)
+            self._parse_columns(parser, db, schema, tables, start_time)
         except Exception:
             self.error_count += 1
-        logger.debug(f"tables: {tables}")
 
-        for table in tables:
+    def _parse_tables(
+        self, parser: Parser, db: str, schema: str, start_time: datetime
+    ) -> List[str]:
+        logger.debug(f"tables: {parser.tables}")
+
+        table_fullnames = []
+        for table in parser.tables:
             fullname = SnowflakeUsageExtractor._built_table_fullname(table, db, schema)
             if not self._filter_table_names(fullname):
                 continue
+            table_fullnames.append(fullname)
 
             if fullname not in self._datasets:
                 self._datasets[fullname] = SnowflakeUsageExtractor._init_dataset(
@@ -178,6 +188,64 @@ class SnowflakeUsageExtractor(BaseExtractor):
             if start_time > utc_now - timedelta(365):
                 self._datasets[fullname].usage.query_counts.last365_days.count += 1
 
+        return table_fullnames
+
+    def _parse_columns(
+        self,
+        parser: Parser,
+        db: str,
+        schema: str,
+        tables: List[str],
+        start_time: datetime,
+    ):
+        logger.debug(f"columns: {parser.columns}")
+        for column in parser.columns:
+            table_name, column_name = SnowflakeUsageExtractor.built_column_fullname(
+                column, db, schema, tables
+            )
+            if column_name == "*" or table_name not in self._datasets:
+                logger.debug(f"table {table_name} column {column_name} passed")
+                continue
+
+            utc_now = datetime.now().replace(tzinfo=timezone.utc)
+            if start_time > utc_now - timedelta(1):
+                self._update_field_query_count(
+                    self._datasets[table_name].usage.field_query_counts.last24_hours,
+                    column_name,
+                )
+
+            if start_time > utc_now - timedelta(7):
+                self._update_field_query_count(
+                    self._datasets[table_name].usage.field_query_counts.last7_days,
+                    column_name,
+                )
+
+            if start_time > utc_now - timedelta(30):
+                self._update_field_query_count(
+                    self._datasets[table_name].usage.field_query_counts.last30_days,
+                    column_name,
+                )
+
+            if start_time > utc_now - timedelta(90):
+                self._update_field_query_count(
+                    self._datasets[table_name].usage.field_query_counts.last90_days,
+                    column_name,
+                )
+
+            if start_time > utc_now - timedelta(365):
+                self._update_field_query_count(
+                    self._datasets[table_name].usage.field_query_counts.last365_days,
+                    column_name,
+                )
+
+    @staticmethod
+    def _update_field_query_count(query_counts: List[FieldQueryCount], column: str):
+        item = next((x for x in query_counts if x.field == column), None)
+        if item:
+            item.count += 1
+        else:
+            query_counts.append(FieldQueryCount(field=column, count=1.0))
+
     def _filter_table_names(self, table_fullname: str) -> bool:
         """Filter table names based on included/excluded table names in config"""
 
@@ -198,6 +266,7 @@ class SnowflakeUsageExtractor(BaseExtractor):
 
     @staticmethod
     def _built_table_fullname(table: str, db: str, schema: str) -> str:
+        """built table fullname with <DB>.<SCHEMA>.<TABLE>, in lowercase"""
         dots = table.count(".")
         if dots == 0:
             return f"{db}.{schema}.{table}".lower()
@@ -206,40 +275,85 @@ class SnowflakeUsageExtractor(BaseExtractor):
         else:  # should have at most two dots in SQL table name
             return table.lower()
 
+    @staticmethod
+    def built_column_fullname(
+        column: str, db: str, schema: str, tables: List[str]
+    ) -> Tuple[str, str]:
+        """built column fullname with (<DB>.<SCHEMA>.<TABLE>, <COLUMN>), in lowercase"""
+        table, _, column_name = column.rpartition(".")
+        dots = column.count(".")
+        if dots == 0:  # contains only column name
+            if len(tables) != 1:
+                raise TypeError(f"Query should have only 1 table, got {tables}")
+            return tables[0], column_name.lower()
+        elif dots == 1:  # should be at least "table.column"
+            return f"{db}.{schema}.{table}".lower(), column_name.lower()
+        elif dots == 2:
+            return f"{db}.{table}".lower(), column_name.lower()
+        else:  # should have at most three dots in SQL column name: db.schema.table.column
+            return table.lower(), column_name.lower()
+
     def _calculate_statistics(self) -> None:
-        """Calculate statistics for the extracted Dataset usages"""
+        """Calculate statistics for the extracted usage info"""
         datasets = self._datasets.values()
 
-        self.calculate_percentile(
+        self.calculate_table_percentile(
             datasets, lambda dataset: dataset.usage.query_counts.last24_hours
         )
 
-        self.calculate_percentile(
+        self.calculate_table_percentile(
             datasets, lambda dataset: dataset.usage.query_counts.last7_days
         )
 
-        self.calculate_percentile(
+        self.calculate_table_percentile(
             datasets, lambda dataset: dataset.usage.query_counts.last30_days
         )
 
-        self.calculate_percentile(
+        self.calculate_table_percentile(
             datasets, lambda dataset: dataset.usage.query_counts.last90_days
         )
 
-        self.calculate_percentile(
+        self.calculate_table_percentile(
             datasets, lambda dataset: dataset.usage.query_counts.last365_days
         )
 
+        for dataset in datasets:
+            self.calculate_column_percentile(
+                dataset.usage.field_query_counts.last24_hours
+            )
+            self.calculate_column_percentile(
+                dataset.usage.field_query_counts.last7_days
+            )
+            self.calculate_column_percentile(
+                dataset.usage.field_query_counts.last30_days
+            )
+            self.calculate_column_percentile(
+                dataset.usage.field_query_counts.last90_days
+            )
+            self.calculate_column_percentile(
+                dataset.usage.field_query_counts.last365_days
+            )
+
     @staticmethod
-    def calculate_percentile(
+    def calculate_table_percentile(
         datasets: Collection[Dataset], get_query_count: Callable[[Dataset], QueryCount]
     ) -> None:
         counts = [get_query_count(dataset).count for dataset in datasets]
-        counts.sort()
+        counts.sort(reverse=True)
 
         for dataset in datasets:
             query_count = get_query_count(dataset)
-            query_count.percentile = counts.index(query_count.count) / len(datasets)
+            query_count.percentile = 1.0 - counts.index(query_count.count) / len(
+                datasets
+            )
+
+    @staticmethod
+    def calculate_column_percentile(columns: List[FieldQueryCount]) -> None:
+        counts = [column.count for column in columns]
+        counts.sort(reverse=True)
+
+        for column in columns:
+            column.percentile = 1.0 - counts.index(column.count) / len(columns)
 
     @staticmethod
     def _init_dataset(full_name: str) -> Dataset:
@@ -258,6 +372,13 @@ class SnowflakeUsageExtractor(BaseExtractor):
             last30_days=QueryCount(count=0.0),
             last90_days=QueryCount(count=0.0),
             last365_days=QueryCount(count=0.0),
+        )
+        dataset.usage.field_query_counts = FieldQueryCounts(
+            last24_hours=[],
+            last7_days=[],
+            last30_days=[],
+            last90_days=[],
+            last365_days=[],
         )
 
         return dataset
--- tests/snowflake/usage/test_statistics.py
+++ tests/snowflake/usage/test_statistics.py
@@ -3,6 +3,7 @@ from typing import List
 from metaphor.models.metadata_change_event import (
     Dataset,
     DatasetUsage,
+    FieldQueryCount,
     QueryCount,
     QueryCounts,
 )
@@ -23,37 +24,41 @@ def make_dataset_with_usage(counts: List[int]):
     return dataset
 
 
-def test_percentile():
+def test_table_percentile():
     dataset1 = make_dataset_with_usage([1, 3, 6, 8, 10])
     dataset2 = make_dataset_with_usage([2, 3, 5, 10, 20])
     dataset3 = make_dataset_with_usage([10, 20, 30, 40, 50])
 
-    SnowflakeUsageExtractor.calculate_percentile(
+    SnowflakeUsageExtractor.calculate_table_percentile(
         [dataset1, dataset2, dataset3],
         lambda dataset: dataset.usage.query_counts.last24_hours,
     )
 
-    SnowflakeUsageExtractor.calculate_percentile(
+    SnowflakeUsageExtractor.calculate_table_percentile(
         [dataset1, dataset2, dataset3],
         lambda dataset: dataset.usage.query_counts.last7_days,
     )
 
     # check the calculated percentile for 24 hours
     assert dataset1.usage.query_counts.last24_hours == QueryCount(
-        count=1, percentile=0.0
+        count=1, percentile=0.33333333333333337
     )
     assert dataset2.usage.query_counts.last24_hours == QueryCount(
-        count=2, percentile=0.3333333333333333
+        count=2, percentile=0.6666666666666667
     )
     assert dataset3.usage.query_counts.last24_hours == QueryCount(
-        count=10, percentile=0.6666666666666666
+        count=10, percentile=1.0
     )
 
     # check the calculated percentile for 7 days
-    assert dataset1.usage.query_counts.last7_days == QueryCount(count=3, percentile=0.0)
-    assert dataset2.usage.query_counts.last7_days == QueryCount(count=3, percentile=0.0)
+    assert dataset1.usage.query_counts.last7_days == QueryCount(
+        count=3, percentile=0.6666666666666667
+    )
+    assert dataset2.usage.query_counts.last7_days == QueryCount(
+        count=3, percentile=0.6666666666666667
+    )
     assert dataset3.usage.query_counts.last7_days == QueryCount(
-        count=20, percentile=0.6666666666666666
+        count=20, percentile=1.0
     )
 
     # the percentile will be None since we didn't call the calculation for 30 days
@@ -62,35 +67,51 @@ def test_percentile():
     assert dataset3.usage.query_counts.last30_days == QueryCount(count=30)
 
 
-def test_percentile_only_1():
+def test_table_percentile_only_1():
     dataset1 = make_dataset_with_usage([1, 2, 3, 4, 5])
 
-    SnowflakeUsageExtractor.calculate_percentile(
+    SnowflakeUsageExtractor.calculate_table_percentile(
         [dataset1], lambda dataset: dataset.usage.query_counts.last24_hours
     )
-    SnowflakeUsageExtractor.calculate_percentile(
+    SnowflakeUsageExtractor.calculate_table_percentile(
         [dataset1], lambda dataset: dataset.usage.query_counts.last7_days
     )
-    SnowflakeUsageExtractor.calculate_percentile(
+    SnowflakeUsageExtractor.calculate_table_percentile(
         [dataset1], lambda dataset: dataset.usage.query_counts.last30_days
     )
-    SnowflakeUsageExtractor.calculate_percentile(
+    SnowflakeUsageExtractor.calculate_table_percentile(
         [dataset1], lambda dataset: dataset.usage.query_counts.last90_days
     )
-    SnowflakeUsageExtractor.calculate_percentile(
+    SnowflakeUsageExtractor.calculate_table_percentile(
         [dataset1], lambda dataset: dataset.usage.query_counts.last365_days
     )
 
     assert dataset1.usage.query_counts.last24_hours == QueryCount(
-        count=1, percentile=0.0
+        count=1, percentile=1.0
     )
-    assert dataset1.usage.query_counts.last7_days == QueryCount(count=2, percentile=0.0)
+    assert dataset1.usage.query_counts.last7_days == QueryCount(count=2, percentile=1.0)
     assert dataset1.usage.query_counts.last30_days == QueryCount(
-        count=3, percentile=0.0
+        count=3, percentile=1.0
     )
     assert dataset1.usage.query_counts.last90_days == QueryCount(
-        count=4, percentile=0.0
+        count=4, percentile=1.0
     )
     assert dataset1.usage.query_counts.last365_days == QueryCount(
-        count=5, percentile=0.0
+        count=5, percentile=1.0
     )
+
+
+def test_column_percentile():
+    columns: List[FieldQueryCount] = [
+        FieldQueryCount(count=3),
+        FieldQueryCount(count=1),
+        FieldQueryCount(count=5),
+    ]
+
+    SnowflakeUsageExtractor.calculate_column_percentile(columns)
+
+    assert columns == [
+        FieldQueryCount(count=3, percentile=0.6666666666666667),
+        FieldQueryCount(count=1, percentile=0.33333333333333337),
+        FieldQueryCount(count=5, percentile=1.0),
+    ]
