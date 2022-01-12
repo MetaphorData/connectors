7458d38074b998cf2c6bd1b0f7e34276a736aba3
Scott Huang <scott@metaphor.io>
2022-01-13 00:18:18 +0800

    extend BigQueryUsageRunConfig from BigQueryRunConfig

--- metaphor/bigquery/usage/config.py
+++ metaphor/bigquery/usage/config.py
@@ -1,26 +1,18 @@
 from dataclasses import dataclass
 from dataclasses import field as dataclass_field
-from typing import List, Optional, Set
+from typing import Set
 
 from serde import deserialize
 
-from metaphor.common.extractor import RunConfig
+from metaphor.bigquery.extractor import BigQueryRunConfig
 
 
 @deserialize
 @dataclass
-class BigQueryUsageRunConfig(RunConfig):
-    # Path to service account's JSON key file
-    key_path: str
-
-    # Project ID to use. Use the service account's default project if not set
-    project_id: Optional[str] = None
-
+class BigQueryUsageRunConfig(BigQueryRunConfig):
     lookback_days: int = 30
 
     batch_size: int = 1000
-
-    dataset_filters: List[str] = dataclass_field(default_factory=lambda: [r".*"])
 
     # Query filter to exclude certain usernames from the processing
     excluded_usernames: Set[str] = dataclass_field(default_factory=lambda: set())
