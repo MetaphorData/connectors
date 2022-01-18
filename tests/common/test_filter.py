94a7ce1d00dff6029a326b9ccf01f2d6fe90fee9
Scott Huang <scott@metaphor.io>
2022-01-18 16:23:08 +0800

    Move tests related to filter to common

--- tests/common/test_filter.py
+++ tests/common/test_filter.py
@@ -1,6 +1,18 @@
 from metaphor.common.filter import DatasetFilter, include_table
 
 
+def test_filter_normalization():
+    config = DatasetFilter(
+        includes={"DB": {"schema1": None, "SCHEMA2": set(["TABLE1", "table2"])}},
+        excludes={"db": {"Schema1": None}},
+    )
+
+    assert config.normalize() == DatasetFilter(
+        includes={"db": {"schema1": None, "schema2": set(["table1", "table2"])}},
+        excludes={"db": {"schema1": None}},
+    )
+
+
 def test_include_table_empty_filter():
 
     filter = DatasetFilter(
