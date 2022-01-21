128c2155d6e62d533719f40070a65917fbc7d908
Scott Huang <scott@metaphor.io>
2022-01-21 12:09:22 +0800

    Move SamplingConfig to common

--- metaphor/bigquery/profile/config.py
+++ metaphor/bigquery/profile/config.py
@@ -4,16 +4,7 @@ from typing import Optional
 from serde import deserialize
 
 from metaphor.bigquery.config import BigQueryRunConfig
-
-
-@deserialize
-@dataclass
-class SamplingConfig:
-    # Sampling percentage, i.e. 1 means 1% of rows will be sampled. Value must be between 0 and 100
-    percentage: int
-
-    # Sampling only affect table large than threshold
-    threshold: Optional[int] = None
+from metaphor.common.extractor import SamplingConfig
 
 
 @deserialize
--- metaphor/common/extractor.py
+++ metaphor/common/extractor.py
@@ -31,6 +31,18 @@ class OutputConfig:
 
 @deserialize
 @dataclass
+class SamplingConfig:
+    """Config for profile connector"""
+
+    # Sampling percentage, i.e. 1 means 1% of rows will be sampled. Value must be between 0 and 100
+    percentage: int
+
+    # Sampling only affect table large than threshold
+    threshold: Optional[int] = None
+
+
+@deserialize
+@dataclass
 class RunConfig:
     """Base class for runtime parameters
 
