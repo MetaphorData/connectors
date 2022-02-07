1c7b7994cf5ea05bf97f5e903cd9bed4d0ad0a49
Yi Wang <yi@metaphor.io>
2022-02-07 11:36:20 -0800

    Support dbt 1 0 [sc-5881]

---
+++ metaphor/dbt/manifest_parser_v4.py
@@ -0,0 +1,226 @@
+from typing import Dict, Optional, Union
+
+from metaphor.models.metadata_change_event import (
+    DataPlatform,
+    Dataset,
+    DbtMacro,
+    DbtMacroArgument,
+    DbtMaterialization,
+    DbtMaterializationType,
+    DbtModel,
+    DbtTest,
+    VirtualView,
+)
+from pydantic.utils import unique_list
+
+from metaphor.common.entity_id import dataset_fullname, to_dataset_entity_id
+from metaphor.common.logger import get_logger
+
+from .generated.dbt_manifest_v4 import (
+    CompiledGenericTestNode,
+    CompiledModelNode,
+    DbtManifest,
+    ParsedGenericTestNode,
+    ParsedMacro,
+    ParsedModelNode,
+    ParsedSourceDefinition,
+)
+from .util import (
+    build_source_code_url,
+    get_model_owner_ids,
+    get_virtual_view_id,
+    init_dbt_tests,
+    init_field,
+    init_virtual_view,
+)
+
+logger = get_logger(__name__)
+
+# compiled node has 'compiled_sql' field
+MODEL_NODE_TYPE = Union[CompiledModelNode, ParsedModelNode]
+TEST_NODE_TYPE = Union[CompiledGenericTestNode, ParsedGenericTestNode]
+
+
+class ManifestParserV4:
+    """
+    dbt manifest parser, using v4 schema https://schemas.getdbt.com/dbt/manifest/v4.json
+    """
+
+    def __init__(
+        self,
+        platform: DataPlatform,
+        account: Optional[str],
+        project_source_url: Optional[str],
+        datasets: Dict[str, Dataset],
+        virtual_views: Dict[str, VirtualView],
+    ):
+        self._platform = platform
+        self._account = account
+        self._project_source_url = project_source_url
+        self._datasets = datasets
+        self._virtual_views = virtual_views
+
+    def parse(self, manifest_json: dict) -> None:
+        try:
+            manifest = DbtManifest.parse_obj(manifest_json)
+        except Exception as e:
+            logger.error(f"Parse manifest json error: {e}")
+            raise e
+
+        nodes = manifest.nodes
+        sources = manifest.sources
+        macros = manifest.macros
+
+        models = {
+            k: v
+            for (k, v) in nodes.items()
+            if isinstance(v, (CompiledModelNode, ParsedModelNode))
+            # if upgraded to python 3.8+, can use get_args(MODEL_NODE_TYPE)
+        }
+        tests = {
+            k: v
+            for (k, v) in nodes.items()
+            if isinstance(v, (CompiledGenericTestNode, ParsedGenericTestNode))
+        }
+
+        self._parse_manifest_nodes(sources, macros, tests, models)
+
+    def _parse_manifest_nodes(
+        self,
+        sources: Dict[str, ParsedSourceDefinition],
+        macros: Dict[str, ParsedMacro],
+        tests: Dict[str, TEST_NODE_TYPE],
+        models: Dict[str, MODEL_NODE_TYPE],
+    ) -> None:
+        source_map = {}
+        for key, source in sources.items():
+            assert source.database is not None
+            source_map[key] = to_dataset_entity_id(
+                dataset_fullname(source.database, source.schema_, source.identifier),
+                self._platform,
+                self._account,
+            )
+
+        macro_map = {}
+        for key, macro in macros.items():
+            arguments = (
+                [
+                    DbtMacroArgument(
+                        name=arg.name,
+                        type=arg.type,
+                        description=arg.description,
+                    )
+                    for arg in macro.arguments
+                ]
+                if macro.arguments
+                else []
+            )
+
+            macro_map[key] = DbtMacro(
+                name=macro.name,
+                unique_id=macro.unique_id,
+                package_name=macro.package_name,
+                description=macro.description,
+                arguments=arguments,
+                sql=macro.macro_sql,
+                depends_on_macros=macro.depends_on.macros if macro.depends_on else None,
+            )
+
+        for _, model in models.items():
+            init_virtual_view(self._virtual_views, model.unique_id)
+
+        for key, model in models.items():
+            virtual_view = init_virtual_view(self._virtual_views, model.unique_id)
+            virtual_view.dbt_model = DbtModel(
+                package_name=model.package_name,
+                description=model.description,
+                url=build_source_code_url(
+                    self._project_source_url, model.original_file_path
+                ),
+                tags=model.tags,
+                raw_sql=model.raw_sql,
+                fields=[],
+            )
+            dbt_model = virtual_view.dbt_model
+
+            if isinstance(model, CompiledModelNode):
+                dbt_model.compiled_sql = model.compiled_sql
+
+            dbt_model.owners = get_model_owner_ids(model)
+
+            assert model.config is not None and model.database is not None
+            materialized = model.config.materialized
+
+            if materialized:
+                try:
+                    materialization_type = DbtMaterializationType[materialized.upper()]
+                except KeyError:
+                    materialization_type = DbtMaterializationType.OTHER
+
+                dbt_model.materialization = DbtMaterialization(
+                    type=materialization_type,
+                    target_dataset=str(
+                        to_dataset_entity_id(
+                            dataset_fullname(
+                                model.database, model.schema_, model.alias or model.name
+                            ),
+                            self._platform,
+                            self._account,
+                        )
+                    ),
+                )
+
+            if model.columns is not None:
+                for col in model.columns.values():
+                    column_name = col.name.lower()
+                    field = init_field(dbt_model.fields, column_name)
+                    field.description = col.description
+                    field.native_type = col.data_type or "Not Set"
+
+            if model.depends_on is not None:
+                if model.depends_on.nodes:
+                    dbt_model.source_models = unique_list(
+                        [
+                            get_virtual_view_id(self._virtual_views[n].logical_id)
+                            for n in model.depends_on.nodes
+                            if n.startswith("model.")
+                        ]
+                    )
+
+                    dbt_model.source_datasets = unique_list(
+                        [
+                            str(source_map[n])
+                            for n in model.depends_on.nodes
+                            if n.startswith("source.")
+                        ]
+                    )
+
+                if model.depends_on.macros:
+                    dbt_model.macros = [macro_map[n] for n in model.depends_on.macros]
+
+        for key, test in tests.items():
+            # check test is referring a model
+            if test.depends_on is None or not test.depends_on.nodes:
+                continue
+
+            model_unique_id = test.depends_on.nodes[0]
+            if not model_unique_id.startswith("model."):
+                continue
+
+            columns = []
+            if test.columns:
+                columns = list(test.columns.keys())
+            elif test.column_name:
+                columns = [test.column_name]
+
+            dbt_test = DbtTest(
+                name=test.name,
+                unique_id=test.unique_id,
+                columns=columns,
+                depends_on_macros=test.depends_on.macros,
+            )
+
+            if isinstance(test, CompiledGenericTestNode):
+                dbt_test.sql = test.compiled_sql
+
+            init_dbt_tests(self._virtual_views, model_unique_id).append(dbt_test)
---
+++ metaphor/dbt/util.py
@@ -0,0 +1,168 @@
+import re
+from email.utils import parseaddr
+from typing import Dict, List, Optional, Union
+
+from metaphor.models.metadata_change_event import (
+    DataPlatform,
+    Dataset,
+    DatasetDocumentation,
+    DatasetLogicalID,
+    DatasetSchema,
+    DbtTest,
+    FieldDocumentation,
+    SchemaField,
+    SchemaType,
+    VirtualView,
+    VirtualViewLogicalID,
+    VirtualViewType,
+)
+from pydantic.utils import unique_list
+
+from metaphor.common.entity_id import (
+    EntityId,
+    dataset_fullname,
+    to_dataset_entity_id,
+    to_person_entity_id,
+    to_virtual_view_entity_id,
+)
+from metaphor.dbt.generated.dbt_manifest_v3 import (
+    CompiledModelNode as CompiledModelNode_v3,
+)
+from metaphor.dbt.generated.dbt_manifest_v3 import ParsedModelNode as ParsedModelNode_v3
+from metaphor.dbt.generated.dbt_manifest_v4 import (
+    CompiledModelNode as CompiledModelNode_v4,
+)
+from metaphor.dbt.generated.dbt_manifest_v4 import ParsedModelNode as ParsedModelNode_v4
+
+MODEL_NODE_TYPE = Union[
+    CompiledModelNode_v3, ParsedModelNode_v3, CompiledModelNode_v4, ParsedModelNode_v4
+]
+
+
+def get_dataset_entity_id(self, db: str, schema: str, table: str) -> EntityId:
+    return to_dataset_entity_id(
+        dataset_fullname(db, schema, table), self._platform, self._account
+    )
+
+
+def get_virtual_view_id(logical_id: VirtualViewLogicalID) -> str:
+    return str(to_virtual_view_entity_id(logical_id.name, logical_id.type))
+
+
+def get_model_name_from_unique_id(unique_id: str) -> str:
+    assert unique_id.startswith("model."), f"invalid model id {unique_id}"
+    return unique_id[6:]
+
+
+def get_model_owner_ids(
+    model: MODEL_NODE_TYPE, owner_key="owner"
+) -> Optional[List[str]]:
+    # v3 use 'model.config.meta' while v1, v2 use 'model.meta'
+    if model.config and model.config.meta and owner_key in model.config.meta:
+        owners = model.config.meta[owner_key]
+    elif model.meta and owner_key in model.meta:
+        owners = model.meta[owner_key]
+    else:
+        return None
+
+    parts = re.split(r"(\s|,)", owners.strip())
+    return unique_list(
+        [str(to_person_entity_id(p)) for p in parts if "@" in parseaddr(p)[1]]
+    )
+
+
+def init_dataset(
+    datasets: Dict[str, Dataset],
+    database: str,
+    schema: str,
+    name: str,
+    platform: DataPlatform,
+    account: Optional[str],
+    unique_id: str,
+) -> Dataset:
+    if unique_id not in datasets:
+        datasets[unique_id] = Dataset(
+            logical_id=DatasetLogicalID(
+                name=dataset_fullname(database, schema, name),
+                platform=platform,
+                account=account,
+            )
+        )
+    return datasets[unique_id]
+
+
+def init_virtual_view(
+    virtual_views: Dict[str, VirtualView], unique_id: str
+) -> VirtualView:
+    if unique_id not in virtual_views:
+        virtual_views[unique_id] = VirtualView(
+            logical_id=VirtualViewLogicalID(
+                name=get_model_name_from_unique_id(unique_id),
+                type=VirtualViewType.DBT_MODEL,
+            ),
+        )
+    return virtual_views[unique_id]
+
+
+def init_dbt_tests(
+    virtual_views: Dict[str, VirtualView], dbt_model_unique_id: str
+) -> List[DbtTest]:
+    assert dbt_model_unique_id in virtual_views
+
+    dbt_model = virtual_views[dbt_model_unique_id].dbt_model
+    if dbt_model.tests is None:
+        dbt_model.tests = []
+    return dbt_model.tests
+
+
+def init_schema(dataset: Dataset) -> None:
+    if not dataset.schema:
+        dataset.schema = DatasetSchema()
+        dataset.schema.schema_type = SchemaType.SQL
+        dataset.schema.fields = []
+
+
+def init_field(fields: List[SchemaField], column: str) -> SchemaField:
+    field = next((f for f in fields if f.field_path == column), None)
+    if not field:
+        field = SchemaField(field_path=column)
+        fields.append(field)
+    return field
+
+
+def init_documentation(dataset: Dataset) -> None:
+    if not dataset.documentation:
+        dataset.documentation = DatasetDocumentation()
+        dataset.documentation.dataset_documentations = []
+        dataset.documentation.field_documentations = []
+
+
+def init_field_doc(dataset: Dataset, column: str) -> FieldDocumentation:
+    assert (
+        dataset.documentation is not None
+        and dataset.documentation.field_documentations is not None
+    )
+
+    doc = next(
+        (
+            d
+            for d in dataset.documentation.field_documentations
+            if d.field_path == column
+        ),
+        None,
+    )
+    if not doc:
+        doc = FieldDocumentation()
+        doc.field_path = column
+        dataset.documentation.field_documentations.append(doc)
+    return doc
+
+
+def build_docs_url(docs_base_url: Optional[str], unique_id: str) -> Optional[str]:
+    return f"{docs_base_url}/#!/model/{unique_id}" if docs_base_url else None
+
+
+def build_source_code_url(
+    project_source_url: Optional[str], file_path: str
+) -> Optional[str]:
+    return f"{project_source_url}/{file_path}" if project_source_url else None
