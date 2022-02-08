0b88c1bbbdb230f94aeb36eb8f6f4b2f78d5e59c
Yi (Alan) Wang <yi@metaphor.io>
2022-02-08 12:13:54 -0800

    remove flakehell (#176)
    * remove flakehell
    
    * update ci

--- metaphor/dbt/manifest_parser_v3.py
+++ metaphor/dbt/manifest_parser_v3.py
@@ -13,7 +13,7 @@ from metaphor.models.metadata_change_event import (
 )
 from pydantic.utils import unique_list
 
-from metaphor.common.entity_id import dataset_fullname, to_dataset_entity_id
+from metaphor.common.entity_id import EntityId, dataset_fullname, to_dataset_entity_id
 from metaphor.common.logger import get_logger
 
 from .generated.dbt_manifest_v3 import (
@@ -93,7 +93,7 @@ class ManifestParserV3:
         tests: Dict[str, TEST_NODE_TYPE],
         models: Dict[str, MODEL_NODE_TYPE],
     ) -> None:
-        source_map = {}
+        source_map: Dict[str, EntityId] = {}
         for key, source in sources.items():
             assert source.database is not None
             source_map[key] = to_dataset_entity_id(
@@ -102,7 +102,7 @@ class ManifestParserV3:
                 self._account,
             )
 
-        macro_map = {}
+        macro_map: Dict[str, DbtMacro] = {}
         for key, macro in macros.items():
             arguments = (
                 [
@@ -127,79 +127,14 @@ class ManifestParserV3:
                 depends_on_macros=macro.depends_on.macros if macro.depends_on else None,
             )
 
+        # initialize all virtual views to be used in cross-references
         for _, model in models.items():
             init_virtual_view(self._virtual_views, model.unique_id)
 
-        for key, model in models.items():
-            virtual_view = init_virtual_view(self._virtual_views, model.unique_id)
-            virtual_view.dbt_model = DbtModel(
-                package_name=model.package_name,
-                description=model.description,
-                url=build_source_code_url(
-                    self._project_source_url, model.original_file_path
-                ),
-                tags=model.tags,
-                raw_sql=model.raw_sql,
-                fields=[],
-            )
-            dbt_model = virtual_view.dbt_model
+        for _, model in models.items():
+            self._parse_model(model, source_map, macro_map)
 
-            if isinstance(model, CompiledModelNode):
-                dbt_model.compiled_sql = model.compiled_sql
-
-            dbt_model.owners = get_model_owner_ids(model)
-
-            assert model.config is not None and model.database is not None
-            materialized = model.config.materialized
-
-            if materialized:
-                try:
-                    materialization_type = DbtMaterializationType[materialized.upper()]
-                except KeyError:
-                    materialization_type = DbtMaterializationType.OTHER
-
-                dbt_model.materialization = DbtMaterialization(
-                    type=materialization_type,
-                    target_dataset=str(
-                        to_dataset_entity_id(
-                            dataset_fullname(
-                                model.database, model.schema_, model.alias or model.name
-                            ),
-                            self._platform,
-                            self._account,
-                        )
-                    ),
-                )
-
-            if model.columns is not None:
-                for col in model.columns.values():
-                    column_name = col.name.lower()
-                    field = init_field(dbt_model.fields, column_name)
-                    field.description = col.description
-                    field.native_type = col.data_type or "Not Set"
-
-            if model.depends_on is not None:
-                if model.depends_on.nodes:
-                    dbt_model.source_models = unique_list(
-                        [
-                            get_virtual_view_id(self._virtual_views[n].logical_id)
-                            for n in model.depends_on.nodes
-                            if n.startswith("model.")
-                        ]
-                    )
-
-                    dbt_model.source_datasets = unique_list(
-                        [
-                            str(source_map[n])
-                            for n in model.depends_on.nodes
-                            if n.startswith("source.")
-                        ]
-                    )
-
-                if model.depends_on.macros:
-                    dbt_model.macros = [macro_map[n] for n in model.depends_on.macros]
-
-        for key, test in tests.items():
+        for _, test in tests.items():
             # check test is referring a model
             if test.depends_on is None or not test.depends_on.nodes:
                 continue
@@ -225,3 +160,77 @@ class ManifestParserV3:
                 dbt_test.sql = test.compiled_sql
 
             init_dbt_tests(self._virtual_views, model_unique_id).append(dbt_test)
+
+    def _parse_model(
+        self,
+        model: MODEL_NODE_TYPE,
+        source_map: Dict[str, EntityId],
+        macro_map: Dict[str, DbtMacro],
+    ):
+        virtual_view = init_virtual_view(self._virtual_views, model.unique_id)
+        virtual_view.dbt_model = DbtModel(
+            package_name=model.package_name,
+            description=model.description,
+            url=build_source_code_url(
+                self._project_source_url, model.original_file_path
+            ),
+            tags=model.tags,
+            raw_sql=model.raw_sql,
+            fields=[],
+        )
+        dbt_model = virtual_view.dbt_model
+
+        if isinstance(model, CompiledModelNode):
+            dbt_model.compiled_sql = model.compiled_sql
+
+        dbt_model.owners = get_model_owner_ids(model)
+
+        assert model.config is not None and model.database is not None
+        materialized = model.config.materialized
+
+        if materialized:
+            try:
+                materialization_type = DbtMaterializationType[materialized.upper()]
+            except KeyError:
+                materialization_type = DbtMaterializationType.OTHER
+
+            dbt_model.materialization = DbtMaterialization(
+                type=materialization_type,
+                target_dataset=str(
+                    to_dataset_entity_id(
+                        dataset_fullname(
+                            model.database, model.schema_, model.alias or model.name
+                        ),
+                        self._platform,
+                        self._account,
+                    )
+                ),
+            )
+
+        if model.columns is not None:
+            for col in model.columns.values():
+                column_name = col.name.lower()
+                field = init_field(dbt_model.fields, column_name)
+                field.description = col.description
+                field.native_type = col.data_type or "Not Set"
+
+        if model.depends_on is not None:
+            if model.depends_on.nodes:
+                dbt_model.source_models = unique_list(
+                    [
+                        get_virtual_view_id(self._virtual_views[n].logical_id)
+                        for n in model.depends_on.nodes
+                        if n.startswith("model.")
+                    ]
+                )
+
+                dbt_model.source_datasets = unique_list(
+                    [
+                        str(source_map[n])
+                        for n in model.depends_on.nodes
+                        if n.startswith("source.")
+                    ]
+                )
+
+            if model.depends_on.macros:
+                dbt_model.macros = [macro_map[n] for n in model.depends_on.macros]
