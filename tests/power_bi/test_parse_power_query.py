ac3d2f56a9f0dacc60092973958f2a4fcd85faac
Scott Huang <scott@metaphor.io>
2022-03-10 11:44:54 +0800

    Change test case data

--- tests/power_bi/test_parse_power_query.py
+++ tests/power_bi/test_parse_power_query.py
@@ -6,37 +6,23 @@ from metaphor.power_bi.extractor import PowerBIExtractor
 
 def test_parse_dataset_redshift():
     assert PowerBIExtractor.parse_power_query(
-        'let\n    Source = AmazonRedshift.Database("url:5439","metaphor"),\n    public = Source{[Name="public"]}[Data],\n    sample_sales_records1 = public{[Name="sample_sales_records"]}[Data]\nin\n    sample_sales_records1'
-    ) == to_dataset_entity_id(
-        "metaphor.public.sample_sales_records", platform=DataPlatform.REDSHIFT
-    )
-
-    assert PowerBIExtractor.parse_power_query(
-        'let\n    Source = AmazonRedshift.Database("url:5439","metaphor"),\n    private = Source{[Name="private"]}[Data],\n    test_scott_2 = private{[Name="test_scott_1"]}[Data]\nin\n    test_scott_2'
-    ) == to_dataset_entity_id(
-        "metaphor.private.test_scott_1", platform=DataPlatform.REDSHIFT
-    )
-
-    assert PowerBIExtractor.parse_power_query(
-        'let\n    Source = AmazonRedshift.Database("url:5439","metaphor"),\n    private = Source{[Name="private"]}[Data],\n    test_scott_3 = private{[Name="test_scott_2"]}[Data]\nin\n    test_scott_3'
-    ) == to_dataset_entity_id(
-        "metaphor.private.test_scott_2", platform=DataPlatform.REDSHIFT
-    )
+        'let\n    Source = AmazonRedshift.Database("url:5439","db"),\n    public = Source{[Name="public"]}[Data],\n    table1 = public{[Name="table"]}[Data]\nin\n    table1'
+    ) == to_dataset_entity_id("db.public.table", platform=DataPlatform.REDSHIFT)
 
 
 def test_parse_dataset_snowflake():
     assert PowerBIExtractor.parse_power_query(
-        'let\n    Source = Snowflake.Databases("metaphor-dev.snowflakecomputing.com","COMPUTE_WH"),\n    ACME_Database = Source{[Name="ACME",Kind="Database"]}[Data],\n    SHOPIFY_Schema = ACME_Database{[Name="SHOPIFY",Kind="Schema"]}[Data],\n    TRANSACTION_Table = SHOPIFY_Schema{[Name="TRANSACTION",Kind="Table"]}[Data]\nin\n    TRANSACTION_Table'
+        'let\n    Source = Snowflake.Databases("some-account.snowflakecomputing.com","COMPUTE_WH"),\n    DB_Database = Source{[Name="DB",Kind="Database"]}[Data],\n    PUBLIC_Schema = DB_Database{[Name="PUBLIC",Kind="Schema"]}[Data],\n    TEST_Table = PUBLIC_Schema{[Name="TEST",Kind="Table"]}[Data]\nin\n    TEST_Table'
     ) == to_dataset_entity_id(
-        "acme.shopify.transaction",
+        "db.public.test",
         platform=DataPlatform.SNOWFLAKE,
-        account="metaphor-dev",
+        account="some-account",
     )
 
 
 def test_parse_dataset_bigquery():
     assert PowerBIExtractor.parse_power_query(
-        'let\n    Source = GoogleBigQuery.Database(),\n    #"metaphor-data" = Source{[Name="metaphor-data"]}[Data],\n    test_Schema = #"metaphor-data"{[Name="test",Kind="Schema"]}[Data],\n    yi_test3_Table = test_Schema{[Name="yi_test3",Kind="Table"]}[Data]\nin\n    yi_test3_Table'
+        'let\n    Source = GoogleBigQuery.Database(),\n    #"test-project" = Source{[Name="test-project"]}[Data],\n    test_Schema = #"test-project"{[Name="test",Kind="Schema"]}[Data],\n    example_Table = test_Schema{[Name="example",Kind="Table"]}[Data]\nin\n    example_Table'
     ) == to_dataset_entity_id(
-        "metaphor-data.test.yi_test3", platform=DataPlatform.BIGQUERY
+        "test-project.test.example", platform=DataPlatform.BIGQUERY
     )
