7f4bbcb700edde364cfc7d1e9968eff8cc294453
Scott Ssuyi Huang <scott@metaphor.io>
2022-04-19 22:23:38 +0800

    Update looker-sdk, change LookerAPI version to 4.0 (#247)

--- metaphor/looker/extractor.py
+++ metaphor/looker/extractor.py
@@ -3,8 +3,8 @@ from typing import Collection, Dict, Iterable, List, Sequence, Set, Tuple
 
 try:
     import looker_sdk
-    from looker_sdk.sdk.api31.methods import Looker31SDK
-    from looker_sdk.sdk.api31.models import DashboardElement
+    from looker_sdk.sdk.api40.methods import Looker40SDK
+    from looker_sdk.sdk.api40.models import DashboardElement
 except ImportError:
     print("Please install metaphor[looker] extra\n")
     raise
@@ -57,7 +57,7 @@ class LookerExtractor(BaseExtractor):
         "text": ChartType.TEXT,
     }
 
-    def initSdk(self, config: LookerRunConfig) -> Looker31SDK:
+    def initSdk(self, config: LookerRunConfig) -> Looker40SDK:
         # Load config using environment variables instead from looker.ini file
         # See https://github.com/looker-open-source/sdk-codegen#environment-variable-configuration
         os.environ["LOOKERSDK_BASE_URL"] = config.base_url
@@ -65,7 +65,7 @@ class LookerExtractor(BaseExtractor):
         os.environ["LOOKERSDK_CLIENT_SECRET"] = config.client_secret
         os.environ["LOOKERSDK_VERIFY_SSL"] = str(config.verify_ssl)
         os.environ["LOOKERSDK_TIMEOUT"] = str(config.timeout)
-        return looker_sdk.init31()
+        return looker_sdk.init40()
 
     async def extract(self, config: LookerRunConfig) -> Collection[ENTITY_TYPES]:
         assert isinstance(config, LookerExtractor.config_class())
@@ -91,7 +91,7 @@ class LookerExtractor(BaseExtractor):
         return entities
 
     def _fetch_dashboards(
-        self, config: LookerRunConfig, sdk: Looker31SDK, model_map: Dict[str, Model]
+        self, config: LookerRunConfig, sdk: Looker40SDK, model_map: Dict[str, Model]
     ) -> List[Dashboard]:
         dashboards: List[Dashboard] = []
         for basic_dashboard in sdk.all_dashboards():
