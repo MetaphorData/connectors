e82f0fc0bf5d8d43bc67849fe4ea8f62928cb3a7
Scott Huang <scott@metaphor.io>
2022-05-06 01:20:16 +0800

    Check either password or secret_key is set

--- metaphor/thought_spot/config.py
+++ metaphor/thought_spot/config.py
@@ -1,5 +1,6 @@
 from typing import Optional
 
+from pydantic import validator
 from pydantic.dataclasses import dataclass
 
 from metaphor.common.base_config import BaseConfig
@@ -12,5 +13,11 @@ class ThoughtspotRunConfig(BaseConfig):
     # ThoughtSpot instance url
     base_url: str
 
-    password: Optional[str] = None
     secret_key: Optional[str] = None
+    password: Optional[str] = None
+
+    @validator("password")
+    def check_password_or_secret_key(cls, password, values):
+        if "secret_key" not in values and not password:
+            raise ValueError("Either password or secret_key is required")
+        return password
