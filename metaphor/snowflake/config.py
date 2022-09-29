ee8f572d0c8e6c904f27ef954fac0c4e12924f2b
Yi (Alan) Wang <yi@metaphor.io>
2022-09-29 04:06:11 -0700

    Query log crawler snowflake [sc-12584] (#362)
    * Query log crawler [sc-12584]
    
    * address comments
    
    * fix import
    
    * remove whitespace
    
    * address comment

--- metaphor/snowflake/config.py
+++ metaphor/snowflake/config.py
@@ -1,10 +1,27 @@
 from dataclasses import field
+from typing import Set
 
 from pydantic.dataclasses import dataclass
 
 from metaphor.common.filter import DatasetFilter
 from metaphor.snowflake.auth import SnowflakeAuthConfig
 from metaphor.snowflake.utils import DEFAULT_THREAD_POOL_SIZE
+
+# number of query logs to fetch from Snowflake in one batch
+DEFAULT_QUERY_LOG_FETCH_SIZE = 100000
+
+
+@dataclass
+class SnowflakeQueryLogConfig:
+
+    # Number of days back of query logs to fetch, if 0, don't fetch query logs
+    lookback_days: int = 1
+
+    # Query log filter to exclude certain usernames
+    excluded_usernames: Set[str] = field(default_factory=lambda: set())
+
+    # The number of query logs to fetch from Snowflake in one batch
+    fetch_size: int = DEFAULT_QUERY_LOG_FETCH_SIZE
 
 
 @dataclass
@@ -15,3 +32,6 @@ class SnowflakeRunConfig(SnowflakeAuthConfig):
 
     # Max number of concurrent queries to database
     max_concurrency: int = DEFAULT_THREAD_POOL_SIZE
+
+    # configs for fetching query logs
+    query_log: SnowflakeQueryLogConfig = SnowflakeQueryLogConfig()
