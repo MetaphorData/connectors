4a5aba259f63cca768f8ddfe9138c2b50bd4ad4e
Scott Huang <scott@metaphor.io>
2023-01-12 14:39:43 +0800

    Remove debug output

--- tests/mysql/test_extractor.py
+++ tests/mysql/test_extractor.py
@@ -115,8 +115,4 @@ async def test_extractor(test_root_dir):
         extractor = MySQLExtractor(config)
         events = [EventUtil.trim_event(e) for e in await extractor.extract()]
 
-        import json
-
-        print(json.dumps(events))
-
     assert events == load_json(f"{test_root_dir}/mysql/expected.json")
