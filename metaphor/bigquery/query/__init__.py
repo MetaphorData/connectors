c7d79459575140252bfd0eae040900cfb17f304f
Mars Lan <mars.th.lan@gmail.com>
2023-06-10 05:57:47 -0700

    Drop unused query & usage connectors

--- metaphor/bigquery/query/__init__.py
+++
@@ -1,6 +0,0 @@
-from metaphor.bigquery.query.extractor import BigQueryQueryExtractor
-from metaphor.common.cli import cli_main
-
-
-def main(config_file: str):
-    cli_main(BigQueryQueryExtractor, config_file)
