56ffb57b90d63dd81a9c4d315471e51078613327
Mars Lan <mars.th.lan@gmail.com>
2023-07-15 20:54:38 -0700

    Add region_name to S3StorageConfig

--- tests/common/test_storage.py
+++ tests/common/test_storage.py
@@ -1,7 +1,8 @@
 import os
 import tempfile
+from unittest.mock import ANY, MagicMock, patch
 
-from metaphor.common.storage import LocalStorage, S3Storage
+from metaphor.common.storage import LocalStorage, S3Storage, S3StorageConfig
 
 
 def test_parse_s3_uri():
@@ -29,3 +30,23 @@ def test_local_storage(test_root_dir):
     # mkstemp may create more than 1 temp files
     _, temp_file = tempfile.mkstemp(suffix=".tmp")
     assert len(storage.list_files(os.path.dirname(temp_file), ".tmp")) > 0
+
+
+@patch("metaphor.common.storage.boto3.Session")
+@patch("metaphor.common.storage.assume_role")
+def test_s3_storage_configs(mock_assume_role, mock_session_class, test_root_dir):
+    S3Storage(
+        assume_role_arn="arn",
+        config=S3StorageConfig(
+            aws_access_key_id="id", aws_secret_access_key="secret", region_name="region"
+        ),
+    )
+
+    mock_session = MagicMock()
+    mock_session_class.side_effect = mock_session
+
+    mock_session_class.assert_called_with(
+        aws_access_key_id="id", aws_secret_access_key="secret", region_name="region"
+    )
+
+    mock_assume_role.assert_called_with(ANY, "arn")
