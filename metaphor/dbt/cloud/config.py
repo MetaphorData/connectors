aaa723b690a581facbafcd76718ec08933431b10
Mars Lan <mars.th.lan@gmail.com>
2023-09-11 12:26:42 -0700

    Support fetching metadata from multiple dbt cloud jobs (#591)

--- metaphor/dbt/cloud/config.py
+++ metaphor/dbt/cloud/config.py
@@ -1,6 +1,7 @@
 from dataclasses import field as dataclass_field
-from typing import List
+from typing import List, Optional
 
+from pydantic import root_validator
 from pydantic.dataclasses import dataclass
 
 from metaphor.common.base_config import BaseConfig
@@ -13,11 +14,14 @@ class DbtCloudConfig(BaseConfig):
     # dbt cloud account ID
     account_id: int
 
-    # dbt cloud job ID
-    job_id: int
-
     # Service token for dbt cloud
     service_token: str
+
+    # (Deprecated. Use job_ids instead) dbt cloud job ID
+    job_id: Optional[int] = None
+
+    # dbt cloud job IDs
+    job_ids: List[int] = dataclass_field(default_factory=lambda: [])
 
     # map meta field to ownerships
     meta_ownerships: List[MetaOwnership] = dataclass_field(default_factory=lambda: [])
@@ -27,3 +31,10 @@ class DbtCloudConfig(BaseConfig):
 
     # Base URL for dbt instance
     base_url: str = "https://cloud.getdbt.com"
+
+    @root_validator
+    def set_job_ids_or_job_id(cls, values):
+        if values.get("job_id") is None and not values.get("job_ids"):
+            raise ValueError("Must set job_ids")
+
+        return values
