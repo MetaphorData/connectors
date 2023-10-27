b39d4775531289a691003e5bbd17d78462beb6ca
Tsung-Ju Lii <andy@metaphor.io>
2023-10-27 16:23:55 +0800

    write tests for unity_catalog to make codecov happy

---
+++ tests/unity_catalog/test_utils.py
@@ -0,0 +1,107 @@
+from unittest.mock import MagicMock
+
+import pytest
+from databricks_cli.sdk.api_client import ApiClient
+from requests import HTTPError, Response
+
+from metaphor.unity_catalog.utils import list_column_lineage, list_table_lineage
+
+
+def test_list_table_lineage():
+    client = ApiClient(host="foo")
+    client.perform_query = MagicMock(
+        return_value={
+            "upstreams": [
+                {
+                    "tableInfo": {
+                        "name": "upstream",
+                        "catalog_name": "db",
+                        "schema_name": "schema",
+                    }
+                },
+                {
+                    "tableInfo": {
+                        "name": "upstream2",
+                        "catalog_name": "db",
+                        "schema_name": "schema",
+                    },
+                },
+                {"fileInfo": {"path": "s3://path", "has_permission": True}},
+                {"tableInfo": {"has_permission": False}},
+            ],
+            "downstreams": [],
+        }
+    )
+
+    result = list_table_lineage(client, "table")
+
+    assert not result.downstreams
+    for upstream in result.upstreams:
+        assert (upstream.fileInfo is not None) != (upstream.tableInfo is not None)
+
+
+def test_list_table_lineage_unavailable_service():
+    response = Response()
+    response.status_code = 503
+
+    client = ApiClient(host="foo")
+    client.perform_query = MagicMock(side_effect=HTTPError(response=response))
+
+    result = list_table_lineage(client, "foo")
+    assert not result.upstreams
+    assert not result.downstreams
+
+
+def test_list_table_lineage_internal_server_error():
+    response = Response()
+    response.status_code = 500
+
+    client = ApiClient(host="foo")
+    client.perform_query = MagicMock(side_effect=HTTPError(response=response))
+
+    with pytest.raises(HTTPError):
+        list_table_lineage(client, "foo")
+
+
+def test_list_column_lineage():
+    client = ApiClient(host="foo")
+    client.perform_query = MagicMock(
+        return_value={
+            "upstream_cols": [
+                {
+                    "name": "col1",
+                    "catalog_name": "db",
+                    "schema_name": "schema",
+                    "table_name": "upstream",
+                }
+            ],
+            "downstream_cols": [],
+        }
+    )
+
+    result = list_column_lineage(client, "table", "col1")
+    assert not result.downstream_cols
+    assert all(col.name == "col1" for col in result.upstream_cols)
+
+
+def test_list_column_lineage_unavailable_service():
+    response = Response()
+    response.status_code = 503
+
+    client = ApiClient(host="foo")
+    client.perform_query = MagicMock(side_effect=HTTPError(response=response))
+
+    result = list_column_lineage(client, "foo", "bar")
+    assert not result.upstream_cols
+    assert not result.downstream_cols
+
+
+def test_list_column_lineage_internal_server_error():
+    response = Response()
+    response.status_code = 500
+
+    client = ApiClient(host="foo")
+    client.perform_query = MagicMock(side_effect=HTTPError(response=response))
+
+    with pytest.raises(HTTPError):
+        list_column_lineage(client, "foo", "bar")
