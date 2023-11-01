926b3dfd5d0511b4649f301d6a83bf1fd0cd14d9
Tsung-Ju Lii <andy@metaphor.io>
2023-11-01 09:59:14 +0800

    Get query logs from unity catalog (#651)
    * enable system table
    
    * upgrade databricks sdk to supported version
    
    * Fix some methods
    
    * logic for getting query logs
    
    * add configs for query logs
    
    * remove unneeded dependency
    
    * why does it not install databricks sdk
    
    * remove funny comment
    
    Signed-off-by: Tsung-Ju Lii <andy@metaphor.io>
    
    * what??????
    
    * fix test
    
    * query log configs should look similar to other components
    
    * add test
    
    * add more tests
    
    * rename methods
    
    * don't need to enable system.access
    
    * change readme
    
    ---------
    
    Signed-off-by: Tsung-Ju Lii <andy@metaphor.io>

--- metaphor/unity_catalog/README.md
+++ metaphor/unity_catalog/README.md
@@ -26,10 +26,28 @@ output:
 
 See [Output Config](../common/docs/output.md) for more information on `output`.
 
-
 ### Optional Configurations
 
 See [Filter Configurations](../common/docs/filter.md) for more information on the optional `filter` config.
+
+### Query Logs
+
+By default, the Unity Catalog connector will fetch a full day's query logs from yesterday, to be analyzed for additional metadata, such as dataset usage and lineage information. To backfill log data, one can set `lookback_days` to the desired value. To turn off query log fetching, set `lookback_days` to 0.  
+
+```yaml
+
+query_log:
+  # (Optional) Number of days of query logs to fetch. Default to 1. If 0, the no query logs will be fetched.
+  lookback_days: <days>
+    
+  # (Optional) A list of users whose queries will be excluded from the log fetching.
+  excluded_usernames:
+    - <user_name1>
+    - <user_name2>
+
+  # (Optional) Limit the number of results returned in one page of query log history. The default is 100.
+  max_results: <count>
+```
 
 ## Testing
 
--- metaphor/unity_catalog/utils.py
+++ metaphor/unity_catalog/utils.py
@@ -1,8 +1,14 @@
-from databricks_cli.sdk.api_client import ApiClient
+import datetime
+import json
+
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.core import ApiClient
+from databricks.sdk.service.sql import QueryFilter, TimeRange
 from pydantic import parse_obj_as
 from requests import HTTPError
 
 from metaphor.common.logger import json_dump_to_debug_file
+from metaphor.unity_catalog.config import UnityCatalogQueryLogConfig
 from metaphor.unity_catalog.models import ColumnLineage, TableLineage
 
 
@@ -11,8 +17,8 @@ def list_table_lineage(client: ApiClient, table_name: str) -> TableLineage:
     resp = None
 
     try:
-        resp = client.perform_query(
-            "GET", "/lineage-tracking/table-lineage", data=_data, version="2.0"
+        resp = client.do(
+            "GET", "/api/2.0/lineage-tracking/table-lineage", data=json.dumps(_data)
         )
         json_dump_to_debug_file(resp, f"table-lineage-{table_name}.json")
         return parse_obj_as(TableLineage, resp)
@@ -31,8 +37,8 @@ def list_column_lineage(
 
     # Lineage API returns 503 on GCP as it's not yet available
     try:
-        resp = client.perform_query(
-            "GET", "/lineage-tracking/column-lineage", data=_data, version="2.0"
+        resp = client.do(
+            "GET", "/api/2.0/lineage-tracking/column-lineage", data=json.dumps(_data)
         )
         json_dump_to_debug_file(resp, f"column-lineage-{table_name}-{column_name}.json")
         return parse_obj_as(ColumnLineage, resp)
@@ -42,3 +48,26 @@ def list_column_lineage(
             return ColumnLineage()
 
         raise e
+
+
+def build_query_log_filter_by(
+    config: UnityCatalogQueryLogConfig,
+    client: WorkspaceClient,
+) -> QueryFilter:
+    end_time = datetime.datetime.now(tz=datetime.timezone.utc)
+    start_time = end_time - datetime.timedelta(days=config.lookback_days)
+
+    query_filter = QueryFilter(
+        query_start_time_range=TimeRange(
+            end_time_ms=int(end_time.timestamp() * 1000),
+            start_time_ms=int(start_time.timestamp() * 1000),
+        )
+    )
+    if config.excluded_usernames:
+        query_filter.user_ids = [
+            user.id
+            for user in client.users.list()
+            if user.user_name not in config.excluded_usernames
+        ]
+
+    return query_filter
