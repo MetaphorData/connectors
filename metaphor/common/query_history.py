f6792449e35b47bdd7cbd63e1f9131ee222c3fb4
Mars Lan <mars.th.lan@gmail.com>
2023-11-06 10:31:16 -0800

    Set email field in UC query log if it contains a valid email address (#662)

--- metaphor/common/query_history.py
+++ metaphor/common/query_history.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Optional, Tuple
 
-from metaphor.common.utils import chunks
+from metaphor.common.utils import chunks, is_email
 from metaphor.models.metadata_change_event import QueryLog, QueryLogs
 
 # max number of query logs to output in one MCE
@@ -15,3 +15,10 @@ def chunk_query_logs(query_logs: List[QueryLog]) -> List[QueryLogs]:
         QueryLogs(logs=chunk)
         for chunk in chunks(query_logs, DEFAULT_QUERY_LOG_OUTPUT_SIZE)
     ]
+
+
+def user_id_or_email(maybe_email: str) -> Tuple[Optional[str], Optional[str]]:
+    """
+    Return a user_id or email depending on if the input contains an email address
+    """
+    return (None, maybe_email) if is_email(maybe_email) else (maybe_email, None)
