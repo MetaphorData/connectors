90b60937585bb1264bd14e5fdb7b75b2ad3a0af8
Mars Lan <mars.th.lan@gmail.com>
2023-11-07 13:28:43 -0800

    Sanitize debug log file names to avoid crashing (#670)
    * Sanitize debug log file names to avoid crashing
    
    * Bump version
    
    * Fix test

--- tests/common/test_logger.py
+++ tests/common/test_logger.py
@@ -1,3 +1,4 @@
+from os.path import basename
 from pathlib import Path
 
 from metaphor.common.logger import json_dump_to_debug_file
@@ -8,3 +9,10 @@ def test_dump_to_debug_file():
     out_file = json_dump_to_debug_file(value, "test")
 
     assert Path(out_file).read_text() == '{"foo": "bar"}'
+
+
+def test_dump_to_debug_file_sanitize_file_name():
+    value = {"foo": "bar"}
+    out_file = json_dump_to_debug_file(value, "illegal/file?name.json")
+
+    assert basename(out_file) == "illegalfilename.json"
