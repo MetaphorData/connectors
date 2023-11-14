e945473af74fd5a22fe2e3d4824631358a9f84d3
Tsung-Ju Lii <andy@metaphor.io>
2023-11-14 21:55:42 +0800

    finish v0 implementation

--- metaphor/unity_catalog/extractor.py
+++ metaphor/unity_catalog/extractor.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 import json
 import logging
 import urllib.parse
@@ -26,6 +27,7 @@ from metaphor.models.metadata_change_event import (
     DatasetLogicalID,
     DatasetSchema,
     DatasetStructure,
+    DatasetStatistics,
     DatasetUpstream,
     EntityUpstream,
     FieldMapping,
@@ -149,10 +151,28 @@ class UnityCatalogExtractor(BaseExtractor):
         for table in tables:
             yield table
 
-    def _init_dataset(self, table: TableInfo) -> Dataset:
-        table_name = table.name
-        schema_name = table.schema_name
-        database = table.catalog_name
+    def _read_dataset_statistics(self, table_info: TableInfo) -> DatasetStatistics:
+        dataset_statistics = DatasetStatistics()
+        if table_info.table_type is not TableType.VIEW:
+            with self._connection.cursor() as cursor:
+                # FIXME this can take a while
+                cursor.execute(f"ANALYZE TABLE {table_info.full_name} COMPUTE STATISTICS")
+                cursor.execute(f"DESCRIBE TABLE EXTENDED {table_info.full_name}")
+                rows = cursor.fetchall()
+                statistics = next((row for row in rows if row.col_name == 'Statistics'), None)
+                if statistics:
+                    # `statistics.data_type` looks like "X bytes, Y rows"
+                    bytes_count, row_count = [float(x) for x in re.findall("(\d+)", statistics.data_type)]
+                    dataset_statistics.data_size_bytes = bytes_count
+                    dataset_statistics.record_count = row_count
+
+        return dataset_statistics
+
+    
+    def _init_dataset(self, table_info: TableInfo) -> Dataset:
+        table_name = table_info.name
+        schema_name = table_info.schema_name
+        database = table_info.catalog_name
 
         normalized_name = dataset_normalized_name(database, schema_name, table_name)
 
@@ -166,25 +186,25 @@ class UnityCatalogExtractor(BaseExtractor):
             database=database, schema=schema_name, table=table_name
         )
 
-        if table.table_type is None:
-            raise ValueError(f"Invalid table {table.name}, no table_type found")
+        if table_info.table_type is None:
+            raise ValueError(f"Invalid table {table_info.name}, no table_type found")
 
         fields = []
-        if table.columns is not None:
+        if table_info.columns is not None:
             fields = [
                 extract_schema_field_from_column_info(column_info)
-                for column_info in table.columns
+                for column_info in table_info.columns
             ]
 
         dataset.schema = DatasetSchema(
             schema_type=SchemaType.SQL,
-            description=table.comment,
+            description=table_info.comment,
             fields=fields,
             sql_schema=SQLSchema(
                 materialization=TABLE_TYPE_MATERIALIZATION_TYPE_MAP.get(
-                    table.table_type, MaterializationType.TABLE
+                    table_info.table_type, MaterializationType.TABLE
                 ),
-                table_schema=table.view_definition if table.view_definition else None,
+                table_schema=table_info.view_definition if table_info.view_definition else None,
             ),
         )
 
@@ -194,19 +214,21 @@ class UnityCatalogExtractor(BaseExtractor):
         dataset.source_info = SourceInfo(main_url=f"{self._host}{path}")
 
         dataset.unity_catalog = UnityCatalog(
-            table_type=UnityCatalogTableType[table.table_type.value],
-            data_source_format=table.data_source_format.value
-            if table.data_source_format is not None
+            table_type=UnityCatalogTableType[table_info.table_type.value],
+            data_source_format=table_info.data_source_format.value
+            if table_info.data_source_format is not None
             else None,
-            storage_location=table.storage_location,
-            owner=table.owner,
+            storage_location=table_info.storage_location,
+            owner=table_info.owner,
             properties=[
                 KeyValuePair(key=k, value=json.dumps(v))
-                for k, v in table.properties.items()
+                for k, v in table_info.properties.items()
             ]
-            if table.properties is not None
+            if table_info.properties is not None
             else [],
         )
+
+        dataset.statistics = self._read_dataset_statistics(table_info)
 
         self._datasets[normalized_name] = dataset
 
