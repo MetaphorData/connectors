668f485be942d08d44266b39808692ca5d5e8fbb
Tsung-Ju Lii <andy@metaphor.io>
2023-11-16 02:59:37 +0800

    Extract row count and data size metadata from UnityCatalog (#683)
    * [wip] add sql connector dependency
    
    * finish v0 implementation
    
    * fix test
    
    * bump version
    
    * revert most of poetry lock stuff
    
    * move data stat to separate connector
    
    * add test
    
    * more tests

--- metaphor/unity_catalog/extractor.py
+++ metaphor/unity_catalog/extractor.py
@@ -140,10 +140,10 @@ class UnityCatalogExtractor(BaseExtractor):
         for table in tables:
             yield table
 
-    def _init_dataset(self, table: TableInfo) -> Dataset:
-        table_name = table.name
-        schema_name = table.schema_name
-        database = table.catalog_name
+    def _init_dataset(self, table_info: TableInfo) -> Dataset:
+        table_name = table_info.name
+        schema_name = table_info.schema_name
+        database = table_info.catalog_name
 
         normalized_name = dataset_normalized_name(database, schema_name, table_name)
 
@@ -157,25 +157,27 @@ class UnityCatalogExtractor(BaseExtractor):
             database=database, schema=schema_name, table=table_name
         )
 
-        if table.table_type is None:
-            raise ValueError(f"Invalid table {table.name}, no table_type found")
+        if table_info.table_type is None:
+            raise ValueError(f"Invalid table {table_info.name}, no table_type found")
 
         fields = []
-        if table.columns is not None:
+        if table_info.columns is not None:
             fields = [
                 extract_schema_field_from_column_info(column_info)
-                for column_info in table.columns
+                for column_info in table_info.columns
             ]
 
         dataset.schema = DatasetSchema(
             schema_type=SchemaType.SQL,
-            description=table.comment,
+            description=table_info.comment,
             fields=fields,
             sql_schema=SQLSchema(
                 materialization=TABLE_TYPE_MATERIALIZATION_TYPE_MAP.get(
-                    table.table_type, MaterializationType.TABLE
+                    table_info.table_type, MaterializationType.TABLE
                 ),
-                table_schema=table.view_definition if table.view_definition else None,
+                table_schema=table_info.view_definition
+                if table_info.view_definition
+                else None,
             ),
         )
 
@@ -185,17 +187,17 @@ class UnityCatalogExtractor(BaseExtractor):
         dataset.source_info = SourceInfo(main_url=f"{self._host}{path}")
 
         dataset.unity_catalog = UnityCatalog(
-            table_type=UnityCatalogTableType[table.table_type.value],
-            data_source_format=table.data_source_format.value
-            if table.data_source_format is not None
+            table_type=UnityCatalogTableType[table_info.table_type.value],
+            data_source_format=table_info.data_source_format.value
+            if table_info.data_source_format is not None
             else None,
-            storage_location=table.storage_location,
-            owner=table.owner,
+            storage_location=table_info.storage_location,
+            owner=table_info.owner,
             properties=[
                 KeyValuePair(key=k, value=json.dumps(v))
-                for k, v in table.properties.items()
+                for k, v in table_info.properties.items()
             ]
-            if table.properties is not None
+            if table_info.properties is not None
             else [],
         )
 
---
+++ tests/unity_catalog/profile/test_extractor.py
@@ -0,0 +1,94 @@
+from types import SimpleNamespace
+from unittest.mock import MagicMock, patch
+
+import pytest
+from databricks.sdk.core import DatabricksError
+from databricks.sdk.service.catalog import CatalogInfo, SchemaInfo, TableInfo, TableType
+
+from metaphor.common.base_config import OutputConfig
+from metaphor.common.event_util import EventUtil
+from metaphor.unity_catalog.profile.config import UnityCatalogProfileRunConfig
+from metaphor.unity_catalog.profile.extractor import UnityCatalogProfileExtractor
+from tests.test_utils import load_json
+
+
+def dummy_config():
+    return UnityCatalogProfileRunConfig(
+        host="http://dummy.host",
+        token="",
+        output=OutputConfig(),
+        warehouse_id=None,
+    )
+
+
+@patch(
+    "metaphor.unity_catalog.profile.extractor.UnityCatalogProfileExtractor.create_connection"
+)
+@patch("metaphor.unity_catalog.extractor.UnityCatalogExtractor.create_api")
+@pytest.mark.asyncio
+async def test_extractor(
+    mock_create_api: MagicMock,
+    mock_create_connection: MagicMock,
+    test_root_dir: str,
+):
+    mock_client = MagicMock()
+
+    mock_client.catalogs = MagicMock()
+    mock_client.catalogs.list = MagicMock()
+    mock_client.catalogs.list.return_value = [CatalogInfo(name="catalog")]
+    mock_client.schemas = MagicMock()
+    mock_client.schemas.list = MagicMock()
+    mock_client.schemas.list.return_value = [
+        SchemaInfo(name="schema"),
+    ]
+    mock_client.tables = MagicMock()
+    mock_client.tables.list = MagicMock()
+    mock_client.tables.list.return_value = [
+        TableInfo(
+            name="table", full_name="catalog.schema.table", table_type=TableType.MANAGED
+        ),
+    ]
+    mock_create_api.return_value = mock_client
+
+    mock_rows = [
+        SimpleNamespace(col_name="Statistics", data_type="5566 bytes, 9487 rows")
+    ]
+
+    mock_cursor = MagicMock()
+    mock_cursor.fetchall = MagicMock()
+    mock_cursor.fetchall.return_value = mock_rows
+
+    mock_cursor_ctx = MagicMock()
+    mock_cursor_ctx.__enter__ = MagicMock()
+    mock_cursor_ctx.__enter__.return_value = mock_cursor
+
+    mock_connection = MagicMock()
+    mock_connection.cursor = MagicMock()
+    mock_connection.cursor.return_value = mock_cursor_ctx
+
+    mock_create_connection.return_value = mock_connection
+
+    extractor = UnityCatalogProfileExtractor(dummy_config())
+    events = [EventUtil.trim_event(e) for e in await extractor.extract()]
+
+    assert events == load_json(f"{test_root_dir}/unity_catalog/profile/expected.json")
+
+
+def test_bad_warehouse():
+    client = MagicMock()
+    client.warehouses = MagicMock()
+    client.warehouses.list = MagicMock()
+    client.warehouses.list.return_value = iter([])
+
+    with pytest.raises(ValueError):
+        UnityCatalogProfileExtractor.create_connection(client, "token", None)
+
+    client.warehouses.list.return_value = iter(["530e470a55aeb40d"])
+    client.warehouses.get = MagicMock(
+        side_effect=DatabricksError("SQL warehouse 530e470a55aeb40e does not exist.")
+    )
+
+    with pytest.raises(ValueError):
+        UnityCatalogProfileExtractor.create_connection(
+            client, "token", "530e470a55aeb40e"
+        )
