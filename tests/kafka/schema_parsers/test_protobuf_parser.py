96138608ca410927d5e98713147cd0947c5f5acd
Tsung-Ju Lii <andy@metaphor.io>
2023-12-01 15:39:01 +0800

    refactor tests

--- tests/kafka/schema_parsers/test_protobuf_parser.py
+++ tests/kafka/schema_parsers/test_protobuf_parser.py
@@ -1,69 +1,19 @@
 from metaphor.kafka.schema_parsers.protobuf_parser import ProtobufParser
-from metaphor.models.metadata_change_event import SchemaField
+from tests.test_utils import load_json
 
 
-def test_protobuf_parser(test_root_dir) -> None:
-    proto_file = f"{test_root_dir}/kafka/schema_parsers/address_book.proto"
-    with open(proto_file) as f:
-        schema = ProtobufParser.parse(f.read(), "address_book")
-    assert schema == [
-        SchemaField(
-            field_name="people",
-            field_path="people",
-            native_type="TYPE_MESSAGE",
-            subfields=[
-                SchemaField(
-                    field_name="name",
-                    field_path="people.name",
-                    native_type="TYPE_STRING",
-                ),
-                SchemaField(
-                    field_name="id",
-                    field_path="people.id",
-                    native_type="TYPE_INT32",
-                ),
-                SchemaField(
-                    field_name="email",
-                    field_path="people.email",
-                    native_type="TYPE_STRING",
-                ),
-                SchemaField(
-                    field_name="phones",
-                    field_path="people.phones",
-                    native_type="TYPE_MESSAGE",
-                    subfields=[
-                        SchemaField(
-                            field_name="number",
-                            field_path="people.phones.number",
-                            native_type="TYPE_STRING",
-                        ),
-                        SchemaField(
-                            field_name="type",
-                            field_path="people.phones.type",
-                            native_type="TYPE_ENUM",
-                        ),
-                    ],
-                ),
-                SchemaField(
-                    field_name="last_updated",
-                    field_path="people.last_updated",
-                    native_type="TYPE_MESSAGE",
-                    subfields=[
-                        SchemaField(
-                            field_name="seconds",
-                            field_path="people.last_updated.seconds",
-                            native_type="TYPE_INT64",
-                        ),
-                        SchemaField(
-                            field_name="nanos",
-                            field_path="people.last_updated.nanos",
-                            native_type="TYPE_INT32",
-                        ),
-                    ],
-                ),
-            ],
-        )
-    ]
+def run_test(test_root_dir: str, path: str) -> None:
+    with open(f"{test_root_dir}/kafka/schema_parsers/{path}/input.proto") as f:
+        raw_schema = f.read()
+    schema = ProtobufParser.parse(raw_schema, path)
+    assert schema is not None
+    assert [field.to_dict() for field in schema] == load_json(
+        f"{test_root_dir}/kafka/schema_parsers/{path}/expected.json"
+    )
+
+
+def test_address_book(test_root_dir) -> None:
+    run_test(test_root_dir, "address_book")
 
 
 def test_parse_bad_proto() -> None:
