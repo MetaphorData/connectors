7aa2f73a43ef32e93068dd7f4faae76c71c69bab
Mars Lan <mars.th.lan@gmail.com>
2023-12-05 08:20:17 -0800

    Update datamodel-code-generator (#717)

---
+++ metaphor/dbt/gen_all_models.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && pwd)"
+cd ${SCRIPT_DIR}
+
+MANIFEST_VERSIONS=" \
+  v5 \
+  v6 \
+  v7 \
+  v8 \
+  v9 \
+  v10 \
+"
+
+for version in ${MANIFEST_VERSIONS}; do
+  ./gen_models.sh manifest ${version}
+done
+
+RUN_RESULTS_VERSIONS="\
+  v4 \
+"
+
+for version in ${RUN_RESULTS_VERSIONS}; do
+  ./gen_models.sh run-results ${version}
+done
