d0b6ac8953fd0b89980945cf77f52fcee6c04d9c
Tsung-Ju Lii <andy@metaphor.io>
2023-12-22 15:09:09 +0800

    address comments

--- metaphor/snowflake/profile/extractor.py
+++ metaphor/snowflake/profile/extractor.py
@@ -1,5 +1,6 @@
 import logging
-from typing import Collection, Dict, List, Tuple
+from collections import defaultdict
+from typing import Collection, Dict, List, NamedTuple, Tuple
 
 try:
     from snowflake.connector import ProgrammingError, SnowflakeConnection
@@ -42,6 +43,21 @@ from metaphor.snowflake.utils import (
 logger = get_logger()
 
 logging.getLogger("snowflake.connector").setLevel(logging.WARNING)
+
+
+class Table(NamedTuple):
+    catalog: str
+    schema: str
+    name: str
+
+    @property
+    def full_name(self) -> str:
+        return f"{self.catalog}.{self.schema}.{self.name}"
+
+    @classmethod
+    def from_name(cls, name: str) -> "Table":
+        catalog, schema, table_name = name.split(".")
+        return Table(catalog, schema, table_name)
 
 
 class SnowflakeProfileExtractor(BaseExtractor):
@@ -138,35 +154,44 @@ class SnowflakeProfileExtractor(BaseExtractor):
         tables: Dict[str, DatasetInfo],
     ) -> None:
         # Create a map of full_name => [column_info] from information_schema.columns
-        cursor = connection.cursor()
-        cursor.execute("SHOW TABLES")
-        table_tuples = [(row[1], row[2], row[3]) for row in cursor]
-
-        column_info_map: Dict[str, List[Tuple[str, str]]] = {}
+        column_info_map: Dict[Table, List[Tuple[str, str]]] = defaultdict(list)
         profile_queries = {}
-        for table_name, table_catalog, table_schema in table_tuples:
-            full_name = f"{table_catalog}.{table_schema}.{table_name}"
-            column_info: List[Tuple[str, str]] = []
 
-            cursor.execute(f'SHOW COLUMNS IN "{table_name}"')
-            for row in cursor:
-                column_name = row[2]
-                data_type = row[3]
-                column_info.append((column_name, data_type))
-            column_info_map[full_name] = column_info
+        cursor = connection.cursor()
+        cursor.execute("SHOW COLUMNS")
 
+        # This could include columns in views or stream tables.
+        for row in cursor:
+            (
+                table_name,
+                table_schema,
+                column_name,
+                data_type,
+                _,
+                _,
+                _,
+                _,
+                _,
+                table_catalog,
+                _,
+            ) = row
+            column_info_map[Table(table_catalog, table_schema, table_name)].append(
+                (column_name, data_type)
+            )
+
+        for table, column_info in column_info_map.items():
             row_count = None
             dataset_info = tables.get(
-                normalize_full_dataset_name(full_name)
+                normalize_full_dataset_name(table.full_name)
             )  # FIXME normalized name is case insensitive, make it case sensitive so that this logic works properly
             if dataset_info:
                 row_count = dataset_info.row_count
 
-            profile_queries[full_name] = QueryWithParam(
+            profile_queries[table.full_name] = QueryWithParam(
                 SnowflakeProfileExtractor._build_profiling_query(
                     column_info,
-                    table_schema,
-                    table_name,
+                    table.schema,
+                    table.name,
                     row_count or 0,
                     self._column_statistics,
                     self._sampling,
@@ -178,16 +203,19 @@ class SnowflakeProfileExtractor(BaseExtractor):
         )
 
         for full_name, profile in profiles.items():
-            dataset = self._datasets[
+            # We need to make sure full_name points to an actual table,
+            # instead of view or stream table.
+            dataset = self._datasets.get(
                 normalize_full_dataset_name(full_name)
-            ]  # FIXME normalized name is case insensitive, make it case sensitive so that this logic works properly
+            )  # FIXME normalized name is case insensitive, make it case sensitive so that this logic works properly
 
-            SnowflakeProfileExtractor._parse_profiling_result(
-                column_info_map[full_name],
-                profile[0],
-                dataset,
-                self._column_statistics,
-            )
+            if dataset:
+                SnowflakeProfileExtractor._parse_profiling_result(
+                    column_info_map[Table.from_name(full_name)],
+                    profile[0],
+                    dataset,
+                    self._column_statistics,
+                )
 
     @staticmethod
     def _build_profiling_query(
