3ba8ebed49dc7b7d440ec62506d21c14232daf2b
Tsung-Ju Lii <andy@metaphor.io>
2023-12-27 18:34:17 +0800

    fix minio dep

--- tests/s3/test_extractor.py
+++ tests/s3/test_extractor.py
@@ -1,11 +1,14 @@
 import pytest
-from testcontainers.minio import MinioContainer, Minio
+from testcontainers.minio import Minio, MinioContainer
+
 
 @pytest.fixture(scope="module")
 def minio(test_root_dir: str):
-    container: MinioContainer = MinioContainer().with_volume_mapping(host=f"{test_root_dir}/s3/data", container="/data", mode="rw") # type: ignore
+    # Just read the directory as if it's the actual object storage
+    container: MinioContainer = MinioContainer().with_volume_mapping(host=f"{test_root_dir}/s3/data", container="/data", mode="rw")  # type: ignore
     with container as minio:
         yield minio.get_client()
+
 
 def test_extractor(minio: Minio) -> None:
     pass
