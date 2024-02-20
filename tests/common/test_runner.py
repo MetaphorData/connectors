71c05665daa16e275b138e8c245f4aca0662d734
Tsung-Ju Lii <andy@metaphor.io>
2024-02-21 01:25:18 +0800

    [sc-24626] Add query logs MCE count to CrawlerRunMetadata (#782)
    * [sc-24626] Add query logs MCE count to CrawlerRunMetadata
    
    * bump version

--- tests/common/test_runner.py
+++ tests/common/test_runner.py
@@ -1,14 +1,18 @@
-from typing import Collection, List
+import tempfile
+from typing import Collection, Iterator, List
 
 from metaphor.common.base_config import BaseConfig, OutputConfig
 from metaphor.common.base_extractor import BaseExtractor
 from metaphor.common.event_util import ENTITY_TYPES
+from metaphor.common.file_sink import FileSinkConfig
 from metaphor.common.runner import run_connector
+from metaphor.common.utils import md5_digest
 from metaphor.models.crawler_run_metadata import RunStatus
 from metaphor.models.metadata_change_event import (
     DataPlatform,
     Dataset,
     DatasetLogicalID,
+    QueryLog,
 )
 
 
@@ -36,9 +40,31 @@ def test_run_connector() -> None:
                     self.extend_errors(e)
             return entities
 
+        def collect_query_logs(self) -> Iterator[QueryLog]:
+            query_logs = [
+                QueryLog(
+                    id=f"{DataPlatform.SNOWFLAKE.name}:{query_id}",
+                    query_id=str(query_id),
+                    platform=DataPlatform.SNOWFLAKE,
+                    account="account",
+                    sql=query_text,
+                    sql_hash=md5_digest(query_text.encode("utf-8")),
+                )
+                for query_id, query_text in enumerate(
+                    f"this is query no. {x}" for x in range(5)
+                )
+            ]
+            for query_log in query_logs:
+                yield query_log
+
+    directory = tempfile.mkdtemp()
+    file_sink_config = FileSinkConfig(directory=directory, batch_size_bytes=1000000)
     dummy_connector = DummyConnector(BaseConfig(output=OutputConfig()))
     events, run_metadata = run_connector(
-        dummy_connector, "dummy_connector", "dummy connector"
+        dummy_connector,
+        "dummy_connector",
+        "dummy connector",
+        file_sink_config=file_sink_config,
     )
     assert run_metadata.status is RunStatus.FAILURE
     assert sorted(
