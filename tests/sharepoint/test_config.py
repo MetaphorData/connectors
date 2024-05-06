e8cb598538d431bdbdc474674cdb3f449a0a2873
rishimo <rishimo2012@gmail.com>
2024-05-06 13:45:19 -0700

    Formatting fix

--- tests/sharepoint/test_config.py
+++ tests/sharepoint/test_config.py
@@ -8,14 +8,14 @@ def test_config(test_root_dir):
     )
 
     assert config == SharepointRunConfig(
-        sharepoint_client_id='sharepoint_client_id',
-        sharepoint_client_secret='sharepoint_client_secret',
-        sharepoint_tenant_id='sharepoint_tenant_id',
-        azure_openAI_key='azure_openAI_key',
-        azure_openAI_version='azure_openAI_version',
-        azure_openAI_endpoint='azure_openAI_endpoint',
-        azure_openAI_model='text-embedding-3-small',
-        azure_openAI_model_name='Embedding_3_small',
+        sharepoint_client_id="sharepoint_client_id",
+        sharepoint_client_secret="sharepoint_client_secret",
+        sharepoint_tenant_id="sharepoint_tenant_id",
+        azure_openAI_key="azure_openAI_key",
+        azure_openAI_version="azure_openAI_version",
+        azure_openAI_endpoint="azure_openAI_endpoint",
+        azure_openAI_model="text-embedding-3-small",
+        azure_openAI_model_name="Embedding_3_small",
         include_text=True,
-        output = OutputConfig()
-        )
+        output=OutputConfig(),
+    )
