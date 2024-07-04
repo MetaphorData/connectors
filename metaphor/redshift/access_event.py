a184c587f06642c342ed708da4b56a2c2ee2226c
Tsung-Ju Lii <andy@metaphor.io>
2024-07-04 17:02:30 +0800

    fix the query

--- metaphor/redshift/access_event.py
+++ metaphor/redshift/access_event.py
@@ -5,41 +5,28 @@ from typing import AsyncIterator
 from asyncpg import Connection, Record
 
 REDSHIFT_USAGE_SQL_TEMPLATE = """
-WITH queries as (
+WITH queries AS (
     SELECT
         *,
         SUM(LEN(text)) OVER (PARTITION BY query) AS length
     FROM
         stl_querytext
-), filtered_queries as (
+), filtered AS (
     SELECT
-        *
+        q.query,
+        LISTAGG(
+            CASE
+                WHEN LEN(RTRIM(q.text)) = 0 THEN q.text
+                ELSE RTRIM(q.text)
+            END,
+            ''
+        ) WITHIN GROUP (ORDER BY q.sequence) AS querytxt
     FROM
-        queries
+        queries AS q
     WHERE
-        length < 65536
-), concatenated_queries as (
-    SELECT
-        query,
-        REPLACE(
-            REPLACE(
-                LISTAGG(
-                    CASE
-                        WHEN LEN(RTRIM(text)) = 0 THEN text
-                        ELSE RTRIM(text)
-                    END,
-                    ''
-                )
-                WITHIN GROUP (ORDER BY sequence)
-                OVER (
-                    PARTITION BY userid, xid, pid, query
-                ),
-                '\r', ''
-            ),
-            '\\n', ''
-        ) AS querytxt
-    FROM
-        filtered_queries
+        q.length < 65536
+    GROUP BY
+        q.query
 )
 SELECT DISTINCT
     ss.userid,
@@ -58,24 +45,11 @@ SELECT DISTINCT
 FROM stl_scan ss
     JOIN svv_table_info sti ON ss.tbl = sti.table_id
     JOIN stl_query sq ON ss.query = sq.query
-    JOIN concatenated_queries q ON ss.query = q.query
+    JOIN filtered q ON ss.query = q.query
     JOIN svl_user_info sui ON sq.userid = sui.usesysid
-WHERE sq.aborted = 0
-GROUP BY
-    ss.userid,
-    ss.query,
-    sui.usename,
-    ss.rows,
-    ss.bytes,
-    ss.tbl,
-    sti.database,
-    sti.schema,
-    sti.table,
-    sq.starttime,
-    sq.endtime,
-    sq.aborted,
-    ss.endtime,
-    q.querytxt
+WHERE ss.starttime >= '{start_time}'
+    AND ss.starttime < '{end_time}'
+    AND sq.aborted = 0
 ORDER BY ss.endtime DESC;
 """
 """
