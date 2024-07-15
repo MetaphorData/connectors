030929f7649c3f20b620d2fddf9ff8efd56322f4
alyiwang <yi@metaphor.io>
2024-07-15 16:53:00 -0700

    fix test

--- tests/hive/test_extractor.py
+++ tests/hive/test_extractor.py
@@ -1,4 +1,3 @@
-import json
 import re
 import time
 
@@ -76,8 +75,6 @@ async def test_extractor(test_root_dir: str) -> None:
             events.append(EventUtil.trim_event(entity))
 
         events.sort(key=lambda event: event.get("logicalId", {}).get("name", ""))
-        logger.info(json.dumps(events))
-
         expected = f"{test_root_dir}/hive/expected.json"
 
         assert events == load_json(expected)
