3960fc67a5e4c594e8d219e26de51b923c599bd6
Scott Huang <scott@metaphor.io>
2024-07-25 23:58:37 +0800

    tests

--- tests/common/test_api_request.py
+++ tests/common/test_api_request.py
@@ -16,7 +16,6 @@ def test_get_request_200(mock_get: MagicMock):
     mock_response.status_code = 200
     mock_response.json.return_value = {"foo": "bar"}
     mock_get.return_value = mock_response
-    mock_get.__name__ = "get"
 
     result = make_request("http://test.com", {}, DummyResult)
     assert result.foo == "bar"
@@ -27,7 +26,6 @@ def test_get_request_not_200(mock_get: MagicMock):
     mock_response = MagicMock()
     mock_response.status_code = 404
     mock_get.return_value = mock_response
-    mock_get.__name__ = "get"
 
     try:
         make_request("http://test.com", {}, Dict)
