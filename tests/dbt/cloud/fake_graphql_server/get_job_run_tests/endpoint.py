3f3b432244828dc44a3624aaeb1fa86e11941467
Tsung-Ju Lii <andy@metaphor.io>
2024-07-25 15:14:39 +0800

    refactor tests

---
+++ tests/dbt/cloud/fake_graphql_server/get_job_run_tests/endpoint.py
@@ -0,0 +1,9 @@
+import os
+from typing import Any, Dict
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
+def fake_GetJobRunTests(variables: Dict[str, Any]):
+    with open(f"{dir_path}/jaffle_shop.json") as f:
+        return f.read()
