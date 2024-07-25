cd1237930c6da21e427698fddb5070d4a3318ced
Tsung-Ju Lii <andy@metaphor.io>
2024-07-25 15:47:05 +0800

    add test input

--- tests/dbt/cloud/fake_graphql_server/targets.py
+++ tests/dbt/cloud/fake_graphql_server/targets.py
@@ -1,9 +1,11 @@
 environment_targets = {
     132676: "jaffle_shop",
     12: "metaphor_subscriptions",
+    13: "london_bike_analysis",
 }
 
 job_targets = {
     21: "jaffle_shop",
     22: "metaphor_subscriptions",
+    23: "london_bike_analysis",
 }
