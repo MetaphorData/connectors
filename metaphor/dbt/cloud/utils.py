2da563ea6b167f9c75570a564650d888337f3643
Scott Ssuyi Huang <scott@metaphor.io>
2024-08-30 23:31:59 +0800

    Fix dbt env platform (#972)
    * map adapter type 'POSTGRES' to data_platform
    
    * Bump version

--- metaphor/dbt/cloud/utils.py
+++ metaphor/dbt/cloud/utils.py
@@ -3,14 +3,20 @@ from metaphor.dbt.cloud.discovery_api.generated.get_environment_adapter_type imp
 )
 from metaphor.models.metadata_change_event import DataPlatform
 
+ADAPTER_TYPE_MAPPING = {
+    "DATABRICKS": DataPlatform.UNITY_CATALOG,
+    "POSTGRES": DataPlatform.POSTGRESQL,
+    "SNOWFLAKE": DataPlatform.SNOWFLAKE,
+}
+
 
 def parse_environment(environment: GetEnvironmentAdapterTypeEnvironment):
     adapter_type = (
         environment.adapter_type or "unknown"
     )  # It's possible for the environment to not have an adapter type!
     adapter_type = adapter_type.upper()
-    if adapter_type == "DATABRICKS":
-        platform = DataPlatform.UNITY_CATALOG
+    if adapter_type in ADAPTER_TYPE_MAPPING:
+        platform = ADAPTER_TYPE_MAPPING[adapter_type]
     else:
         assert (
             adapter_type in DataPlatform.__members__
