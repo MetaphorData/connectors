ceaa92e29067f068a060096e8eba287e28293925
Scott Huang <scott@metaphor.io>
2024-09-03 14:15:47 +0800

    Address comments

--- metaphor/common/sql/utils.py
+++ metaphor/common/sql/utils.py
@@ -8,6 +8,6 @@ def is_valid_queried_datasets(datasets: List[QueriedDataset]) -> bool:
     database and schema must exist and not an empty string
     """
     for dataset in datasets:
-        if not dataset.database or not dataset.schema:
+        if not dataset.database or not dataset.schema or dataset.table:
             return False
     return True
