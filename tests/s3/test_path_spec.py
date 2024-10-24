88213810daf443865358e010f7a07187ddc7cd69
Tsung-Ju Lii <andy@metaphor.io>
2024-10-24 22:12:57 +0800

    [sc-29504] `PathSpec._path_is_excluded` to check if path is excluded (#1018)

--- tests/s3/test_path_spec.py
+++ tests/s3/test_path_spec.py
@@ -1,4 +1,4 @@
-import pytest
+import yarl
 
 from metaphor.s3.path_spec import PathSpec
 
@@ -12,9 +12,10 @@ def test_exclude_all_files_in_directory() -> None:
         excludes=["s3://bucket/v1/app/global/*"],
     )
     assert not path_spec.allow_path("s3://bucket/v1/app/global/foo/v4")
+    assert not path_spec.allow_path("s3://bucket/v1/app/global/foo/")
+    assert path_spec.allow_path("s3://bucket/v1/app/foo/bar/")
 
 
-@pytest.mark.skip
 def test_exclude_table_wildcard() -> None:
     path_spec = PathSpec(
         uri="s3://bucket/v1/services/data/{table}/v1/*/{partition[0]}/{partition[1]}/{partition[2]}/*.*",
@@ -24,3 +25,16 @@ def test_exclude_table_wildcard() -> None:
         excludes=["s3://bucket/v1/services/data/skipped_*"],
     )
     assert not path_spec.allow_path("s3://bucket/v1/services/data/skipped_0/")
+    assert not path_spec.allow_path("s3://bucket/v1/services/data/skipped_1")
+
+
+def test_trim_url_end() -> None:
+    assert (
+        PathSpec._trim_url_end(yarl.URL("s3://bucket/foo/bar/")).human_repr()
+        == "s3://bucket/foo/bar"
+    )
+
+    assert (
+        PathSpec._trim_url_end(yarl.URL("s3://bucket/foo/bar/*")).human_repr()
+        == "s3://bucket/foo/bar"
+    )
