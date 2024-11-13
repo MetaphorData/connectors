0678e402bd630ed5b82bfeb59f21230699cc42ec
Scott Huang <scott@metaphor.io>
2024-11-14 00:49:26 +0800

    Support multiple openapi spec

--- metaphor/openapi/config.py
+++ metaphor/openapi/config.py
@@ -1,11 +1,11 @@
-from typing import Optional
+from typing import List, Optional
 
 from pydantic import FilePath, HttpUrl, model_validator
 from pydantic.dataclasses import dataclass
 
 from metaphor.common.base_config import BaseConfig
 from metaphor.common.dataclass import ConnectorConfig
-from metaphor.common.utils import must_set_exactly_one
+from metaphor.common.utils import must_set_at_least_one, must_set_exactly_one
 
 
 @dataclass(config=ConnectorConfig)
@@ -20,13 +20,36 @@ class OpenAPIAuthConfig:
 
 
 @dataclass(config=ConnectorConfig)
-class OpenAPIRunConfig(BaseConfig):
+class OpenAPIJsonConfig:
     base_url: HttpUrl
     openapi_json_path: Optional[FilePath] = None
     openapi_json_url: Optional[HttpUrl] = None
     auth: Optional[OpenAPIAuthConfig] = None
 
     @model_validator(mode="after")
-    def have_path_or_url(self) -> "OpenAPIRunConfig":
+    def have_path_or_url(self) -> "OpenAPIJsonConfig":
         must_set_exactly_one(self.__dict__, ["openapi_json_path", "openapi_json_url"])
+        return self
+
+
+@dataclass(config=ConnectorConfig)
+class OpenAPIRunConfig(BaseConfig):
+    base_url: Optional[HttpUrl] = None
+    openapi_json_path: Optional[FilePath] = None
+    openapi_json_url: Optional[HttpUrl] = None
+    auth: Optional[OpenAPIAuthConfig] = None
+    specs: Optional[List[OpenAPIJsonConfig]] = None
+
+    @model_validator(mode="after")
+    def must_have_at_least_one_spec_config(self) -> "OpenAPIRunConfig":
+        must_set_at_least_one(self.__dict__, ["specs", "base_url"])
+
+        if "base_url" not in self.__dict__ and "specs" in self.__dict__:
+            if not self.specs:
+                raise ValueError("Must have at least one spec")
+
+        if "base_url" in self.__dict__:
+            must_set_exactly_one(
+                self.__dict__, ["openapi_json_path", "openapi_json_url"]
+            )
         return self
