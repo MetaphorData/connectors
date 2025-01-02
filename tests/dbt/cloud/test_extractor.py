22a00b093cb4246b78ae2b06b7ac926ef5b712e4
alyiwang <yi@metaphor.io>
2025-01-02 15:28:54 -0800

    add tests

--- tests/dbt/cloud/test_extractor.py
+++ tests/dbt/cloud/test_extractor.py
@@ -1,3 +1,4 @@
+import json
 from unittest.mock import MagicMock, patch
 
 import pytest
@@ -6,6 +7,7 @@ from metaphor.common.base_config import OutputConfig
 from metaphor.common.event_util import EventUtil
 from metaphor.dbt.cloud.config import DbtCloudConfig
 from metaphor.dbt.cloud.extractor import DbtCloudExtractor
+from metaphor.dbt.config import MetaOwnership
 from tests.dbt.cloud.mock_client import MockAdminClient, MockDiscoveryClient
 from tests.test_utils import load_json
 
@@ -27,9 +29,17 @@ async def test_extractor(
             account_id=123,
             service_token="tok",
             meta_key_tags="my_tags",
+            meta_ownerships=[
+                MetaOwnership(
+                    meta_key="Business Owner",
+                    ownership_type="Business Owner",
+                    email_domain="metaphor.com",
+                )
+            ],
         )
     )
     events = [EventUtil.trim_event(e) for e in await extractor.extract()]
 
     expected = f"{test_root_dir}/dbt/cloud/data/expected.json"
+    print(json.dumps(events))
     assert events == load_json(expected)
