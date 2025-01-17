a80ae3feade52a5a7f4f24747584e87fc5422f1b
alyiwang <yi@metaphor.io>
2025-01-16 23:02:05 -0800

    address comments

--- metaphor/common/snowflake.py
+++ metaphor/common/snowflake.py
@@ -15,10 +15,12 @@ def normalize_snowflake_account(host: str, remove_default_region: bool = False) 
     if host.endswith(SNOWFLAKE_HOST_SUFFIX):
         host = host[: -len(SNOWFLAKE_HOST_SUFFIX)]
 
-    # Strip PrivateLink suffix
+    # Strip PrivateLink suffix, e.g. account.privatelink.snowflakecomputing.com
     if host.endswith(PRIVATE_LINK_SUFFIX):
         host = host[: -len(PRIVATE_LINK_SUFFIX)]
 
+    # Remove default region (us-west-2) if applicable, e.g. account.us-west-2.snowflakecomputing.com
+    # This is to keep the account name consistent with the results from the snowflake crawler
     if remove_default_region and host.endswith(SNOWFLAKE_DEFAULT_REGION):
         host = host[: -len(SNOWFLAKE_DEFAULT_REGION)]
 
