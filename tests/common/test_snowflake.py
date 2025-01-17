a91231c7a1fdf2a10cf890eac1f41e7e5bc60401
alyiwang <yi@metaphor.io>
2025-01-16 23:04:12 -0800

    add test

--- tests/common/test_snowflake.py
+++ tests/common/test_snowflake.py
@@ -6,3 +6,13 @@ def test_normalize_snowflake_account():
     assert normalize_snowflake_account("org-acc") == "org-acc"
     assert normalize_snowflake_account("org.us-west-1") == "org.us-west-1"
     assert normalize_snowflake_account("foo.privatelink") == "foo"
+    assert (
+        normalize_snowflake_account("foo.us-west-2.privatelink.snowflakecomputing.com")
+        == "foo.us-west-2"
+    )
+    assert (
+        normalize_snowflake_account(
+            "foo.us-west-2.privatelink.snowflakecomputing.com", True
+        )
+        == "foo"
+    )
